e3a6938879a4f762927cdbeb172b256715f7eca4
James deBoer <james@huronbox.com>
2013-06-04 20:16:44 -0700

    feat(demo): Start on components

--- demo/web/main.dart
+++ demo/web/main.dart
@@ -28,9 +28,59 @@ class AngularBootstrap {
     // Digest the scope.
     $rootScope.$digest();
   }
-
-
 }
+
+class TabsController implements Controller {
+  Scope $scope;
+  List panes;
+  var selectedPanes = new Expando<dom.Node>();
+
+  MainController(Scope this.$scope) {
+    panes = $scope.panes = [];
+
+    $scope.selected = (pane) {
+      panes.forEach((p) {
+        selectedPanes[p]['selected'] = false;
+      });
+      selectedPanes[pane]['selected'] = true;
+    };
+  }
+
+  addPane(dom.Node pane) {
+    if (panes.length == 0) { $scope.selected(pane); }
+    panes.add(pane);
+  }
+}
+
+class TabsAttrDirective {
+  static var $transclude = "true";
+  static String $template = '<div class="tabbable">Shadow' +
+    '<ul class="nav nav-tabs">' +
+    '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
+    '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
+    '</li>' +
+    '</ul>' +
+    '<content class="tab-content" ng-transclude>CONTENT</content>' +
+    '</div>';
+ // static String $template = '<div>Hello shaddow</div>';
+  BlockList blockList;
+
+
+  TabsAttrDirective(BlockList this.blockList,
+                    dom.Element element) {
+    dom.ShadowRoot shadow = element.createShadowRoot();
+
+
+    print("tab attr: ${element.text}");
+    print("shadow: ${shadow.text}");
+
+  }
+
+  attach(Scope scope) {
+    print("tab attach");
+  }
+}
+
 main() {
   // Set up the Angular directives.
   var module = new Module();
@@ -39,6 +89,8 @@ main() {
   Directives directives = injector.get(Directives);
   directives.register(NgBindAttrDirective);
   directives.register(NgRepeatAttrDirective);
+  directives.register(NgShadowDomAttrDirective);
+  directives.register(TabsAttrDirective);
 
   injector.get(AngularBootstrap)();
 
