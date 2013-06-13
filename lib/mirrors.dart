79cbf362632c0735d576bb01511dcf849a07f613
Pavel Jbanov <pavelgj@gmail.com>
2013-06-13 12:04:37 -0700

    A hack for slow reflectClass

--- lib/mirrors.dart
+++ lib/mirrors.dart
@@ -1,5 +1,17 @@
 part of angular;
 
+Map<Type, ClassMirror> _reflectionCache = new Map<Type, ClassMirror>();
+
+// A hack for slow reflectClass.
+ClassMirror fastReflectClass(Type type) {
+  ClassMirror reflectee = _reflectionCache[type];
+  if (reflectee == null) {
+    reflectee = reflectClass(type);
+    _reflectionCache[type] = reflectee;
+  }
+  return reflectee;
+}
+
 /**
  * A set of functions which build on top of dart:mirrors making them
  * easier to use.
@@ -8,7 +20,7 @@ part of angular;
 // Return the value of a type's static field or null if it is not defined.
 reflectStaticField(Type type, String field) {
   Symbol fieldSym = new Symbol(field);
-  var reflection = reflectClass(type);
+  var reflection = fastReflectClass(type);
   if (!reflection.members.containsKey(fieldSym)) return null;
   if (!reflection.members[fieldSym].isStatic) return null;
 
