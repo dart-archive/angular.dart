41005f8e1b061314641864647831346a2b395466
Vojta Jina <vojta.jina@gmail.com>
2013-07-10 19:03:36 -0700

    fix(ng-bind): allow binding to non string values

--- lib/directives/ng_bind.dart
+++ lib/directives/ng_bind.dart
@@ -3,7 +3,7 @@ part of angular;
 class NgBindAttrDirective {
 
   NgBindAttrDirective(dom.Element element, NodeAttrs attrs, Scope scope) {
-    scope.$watch(attrs[this], (value) => element.text = value);
+    scope.$watch(attrs[this], (value) => element.text = value == null ? '' : value.toString());
   }
 
 }
--- test/directives/ng_bind_spec.dart
+++ test/directives/ng_bind_spec.dart
@@ -1,13 +1,38 @@
 import "../_specs.dart";
+import "../_test_bed.dart";
 
 main() {
   describe('BindDirective', () {
+    TestBed _;
+
+    beforeEach(beforeEachTestBed((tb) => _ = tb));
+
     it('should set text', inject((Scope scope, Injector injector, Compiler compiler) {
       var element = $('<div ng-bind="a"></div>');
       compiler(element)(injector, element);
       scope.a = "abc123";
       scope.$digest();
       expect(element.text()).toEqual('abc123');
+    }));
+
+
+    it('should bind to non string values', inject((Scope scope) {
+      var element = _.compile('<div ng-bind="value"></div>');
+
+      scope.$apply(() {
+        scope['value'] = null;
+      });
+      expect(element.text()).toEqual('');
+
+      scope.$apply(() {
+        scope['value'] = true;
+      });
+      expect(element.text()).toEqual('true');
+
+      scope.$apply(() {
+        scope['value'] = 1;
+      });
+      expect(element.text()).toEqual('1');
     }));
   });
 }
