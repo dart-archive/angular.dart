ce43d185cd46405be9e226038cb48841b4a613d4
Misko Hevery <misko@hevery.com>
2013-07-23 21:09:36 -0700

    chore(block_list): remove factory function

--- lib/block_list.dart
+++ lib/block_list.dart
@@ -1,13 +1,5 @@
 part of angular;
 
-class BlockListFactory {
-  BlockListFactory();
-
-  BlockList call(List<dom.Node> elements, Map<String, BlockType> blockTypes, Injector injector) {
-    return new BlockList(elements, blockTypes, injector);
-  }
-}
-
 /**
  * An Anchor is an instance of a hole. Anchors designate where child Blocks can
  * be added in parent Block. Anchors wrap a DOM element, and act as references
@@ -22,7 +14,8 @@ class BlockList extends ElementWrapper {
   ElementWrapper next;
 
   BlockList(List<dom.Node> this.elements,
-            Map<String, BlockType> this.blockTypes, Injector this.injector) {
+            Map<String, BlockType> this.blockTypes,
+            Injector this.injector) {
   }
 
   Block newBlock(Scope scope, [String type = '']) {
