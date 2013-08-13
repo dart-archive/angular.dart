bcfc59221f9533cdf0aa9303198cd803d9d3856c
Misko Hevery <misko@hevery.com>
2013-08-12 19:22:14 -0700

    docs(NgComponent): document component meta-data

--- lib/directive.dart
+++ lib/directive.dart
@@ -16,13 +16,106 @@ class _NgAnnotationBase {
   });
 }
 
+/**
+ * Meta-data marker placed on a class which should act as a controller for the
+ * component. Angular components are a light-weight version of web-components.
+ * Angular components use shadow-DOM for rendering their templates.
+ */
 class NgComponent extends _NgAnnotationBase {
+  /**
+   * Inlined HTML template for the component.
+   */
   final String template;
+
+  /**
+   * A URL to HTML template. This will be loaded asynchronously and
+   * cached for future component instances.
+   */
   final String templateUrl;
+
+  /**
+   * A CSS URL to load into the shadow DOM.
+   */
   final String cssUrl;
+
+  /**
+   * Use map to define the mapping of component's DOM attributes into
+   * the component instance or scope. The map's key is the DOM attribute name
+   * to map in camelCase (DOM attribute is in dash-case). The Map's value
+   * consists of a mode character, and an expression. If expression is not
+   * specified, it maps to the same scope parameter as is the DOM attribute.
+   *
+   * * `@` - Map the DOM attribute string. The attribute string will be taken
+   *   literally or interpolated if it contains binding {{}} systax.
+   *
+   * * `=` - Treat the DOM attribute value as an expression. Set up a watch
+   *   on both outside as well as component scope to keep the src and
+   *   destination in sync.
+   *
+   * * `&` - Treat the DOM attribute value as an expression. Assign a closure
+   *   function into the component. This allows the component to control
+   *   the invocation of the closure. This is useful for passing
+   *   expressions into controllers which act like callbacks.
+   *
+   * NOTE: an expression may start with `.` which evaluates the expression
+   * against the current controller rather then current scope.
+   *
+   * Example:
+   *
+   *     <my-component title="Hello {{username}}"
+   *                   selection="selectedItem"
+   *                   on-selection-change="doSomething()">
+   *
+   *     @NgComponent(
+   *       selector: 'my-component'
+   *       map: cost {
+   *         'title': '@.title',
+   *         'selection': '=.currentItem',
+   *         'onSelectionChange': '&.onChange'
+   *       }
+   *     )
+   *     class MyComponent {
+   *       String title;
+   *       var currentItem;
+   *       ParsedFn onChange;
+   *     }
+   *
+   *  The above example shows how all three mapping modes are used.
+   *
+   *  * `@.title` maps the title DOM attribute to the controller `title`
+   *    field. Notice that this maps the content of the attribute, which
+   *    means that it can be used with `{{}}` interpolation.
+   *
+   *  * `=.currentItem` maps the expression (in this case the `selectedItem`
+   *    in the current scope into the `currentItem` in the controller. Notice
+   *    that mapping is bi-directional. A change either in controller or on
+   *    parent scope will result in change of the other.
+   *
+   *  * `&.onChange` maps the expression into tho controllers `onChange`
+   *    field. The result of mapping is a callable function which can be
+   *    invoked at any time by the controller. The invocation of the
+   *    callable function will result in the expression `doSomething()` to
+   *    be executed in the parent context.
+   */
   final Map<String, String> map;
+
+  /**
+   * An expression under which the controller instance will be published into.
+   * This allows the expressions in the template to be referring to controller
+   * instance and its properties.
+   */
   final String publishAs;
+
+  /**
+   * Set the shadow root applyAuthorStyles property. See shadow-DOM
+   * documentation for further details.
+   */
   final bool applyAuthorStyles;
+
+  /**
+   * Set the shadow root resetStyleInheritance property. See shadow-DOM
+   * documentation for further details.
+   */
   final bool resetStyleInheritance;
 
   const NgComponent({
