18da31bbb9d0a54dc56c5708a178e19d49900433
James deBoer <james@huronbox.com>
2013-08-23 12:35:02 -0700

    feat(parser): Throw NoSuchMethod in getter.  Remove try/catch in setter.

--- test/parser_spec.dart
+++ test/parser_spec.dart
@@ -351,7 +351,12 @@ main() {
         scope = {'a': {'b': 23}};
         expect(eval('b')).toBeNull();
         expect(eval('a.x')).toBeNull();
-        expect(eval('a.b.c.d')).toBeNull();
+      });
+
+
+      it('should catch NoSuchMethod', () {
+        scope = {'a': {'b': 23}};
+        expect(() => eval('a.b.c.d')).toThrow('NoSuchMethod');
       });
 
 
@@ -702,7 +707,7 @@ class SetterObject {
   void set setter(x) { setterValue = x; }
 }
 
-class OverloadObject {
+class OverloadObject implements Map {
   var overloadValue;
   operator []=(String name, var value) {
     overloadValue = value;
