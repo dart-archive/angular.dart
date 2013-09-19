890e4d88a0767cddee39de4473a445f1c253cdc0
Misko Hevery <misko@hevery.com>
2013-09-19 14:49:30 -0700

    chore(pert): added callable object pert test

--- perf/inovoke_perf.dart
+++ perf/inovoke_perf.dart
@@ -2,13 +2,15 @@ import '_perf.dart';
 import 'dart:async';
 
 main() {
-  var handleDirect = (a, b, c) => a + b + c;
-  var handleDirectNamed = ({a, b, c}) => a + b + c;
-  var handleIndirect = (e) => e.a + e.b + e.c;
+  var handleDirect = (a, b, c) => a;
+  var wrap = new Wrap();
+  var handleDirectNamed = ({a, b, c}) => a;
+  var handleIndirect = (e) => e.a;
   var streamC = new StreamController(sync:true);
   var stream = streamC.stream..listen(handleIndirect);
 
   time('direct', () => handleDirect(1, 2, 3) );
+  time('.call', () => wrap(1, 2, 3) );
   time('directNamed', () => handleDirectNamed(a:1, b:2, c:3) );
   time('indirect', () => handleIndirect(new Container(1, 2, 3)) );
   time('stream', () => streamC.add(new Container(1, 2, 3)));
@@ -20,4 +22,8 @@ class Container {
   var c;
 
   Container(this.a, this.b, this.c);
+}
+
+class Wrap {
+  call(a, b, c) => a + b + c;
 }
