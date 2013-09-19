4aee1e9c0c58e34765cc1cb0bd199ee52c77906c
Chirayu Krishnappa <chirayu@chirayuk.com>
2013-09-18 17:59:03 -0700

    fix(ng_events): fix doubleclick event

--- test/directives/ng_events_spec.dart
+++ test/directives/ng_events_spec.dart
@@ -4,54 +4,54 @@ import '../_specs.dart';
 import "../_test_bed.dart";
 import 'dart:html' as dom;
 
+void addTest(String name, [String eventType='MouseEvent', String eventName]) {
+  if (eventName == null) {
+    eventName = name;
+  }
+
+  describe('ng-$name', () {
+    TestBed _;
+
+    beforeEach(beforeEachTestBed((tb) => _ = tb));
+
+    it('should evaluate the expression on $name', inject(() {
+      _.compile('<button ng-$name="abc = true; event = \$event"></button>');
+      _.triggerEvent(_.rootElement, eventName, eventType);
+      expect(_.rootScope['abc']).toEqual(true);
+      expect(_.rootScope['event'] is dom.UIEvent).toEqual(true);
+    }));
+  });
+}
 
 main() {
-  var events = {
-    'blur': 'MouseEvent',
-    'change': 'MouseEvent',
-    'click': 'MouseEvent',
-    'contextmenu': 'MouseEvent',
-    // TODO(chirayu): Fix this.
-    // 'doubleclick': 'MouseEvent',  // Unable to trigger this event in the test code */
-    'dragenter': 'MouseEvent',
-    'dragleave': 'MouseEvent',
-    'dragover': 'MouseEvent',
-    'dragstart': 'MouseEvent',
-    'drop': 'MouseEvent',
-    'focus': 'MouseEvent',
-    'keydown': 'KeyboardEvent',
-    'keypress': 'KeyboardEvent',
-    'keyup': 'KeyboardEvent',
-    'mousedown': 'MouseEvent',
-    'mouseenter': 'MouseEvent',
-    'mouseleave': 'MouseEvent',
-    'mousemove': 'MouseEvent',
-    'mouseout': 'MouseEvent',
-    'mouseover': 'MouseEvent',
-    'mouseup': 'MouseEvent',
-    'mousewheel': 'MouseEvent',
-    'scroll': 'MouseEvent',
+    addTest('blur', 'MouseEvent');
+    addTest('change', 'MouseEvent');
+    addTest('click', 'MouseEvent');
+    addTest('contextmenu', 'MouseEvent');
+    // The event name differs from the ng- directive name.
+    addTest('doubleclick', 'MouseEvent', 'dblclick');
+    addTest('dragenter', 'MouseEvent');
+    addTest('dragleave', 'MouseEvent');
+    addTest('dragover', 'MouseEvent');
+    addTest('dragstart', 'MouseEvent');
+    addTest('drop', 'MouseEvent');
+    addTest('focus', 'MouseEvent');
+    addTest('keydown', 'KeyboardEvent');
+    addTest('keypress', 'KeyboardEvent');
+    addTest('keyup', 'KeyboardEvent');
+    addTest('mousedown', 'MouseEvent');
+    addTest('mouseenter', 'MouseEvent');
+    addTest('mouseleave', 'MouseEvent');
+    addTest('mousemove', 'MouseEvent');
+    addTest('mouseout', 'MouseEvent');
+    addTest('mouseover', 'MouseEvent');
+    addTest('mouseup', 'MouseEvent');
+    addTest('mousewheel', 'MouseEvent');
+    addTest('scroll', 'MouseEvent');
     // These should be of type TouchEvent but that causes the tests to fail.
     // They pass as a MouseEvent.
-    'touchcancel': 'MouseEvent', // 'TouchEvent',
-    'touchend': 'MouseEvent', // 'TouchEvent',
-    'touchmove': 'MouseEvent', // 'TouchEvent',
-    'touchstart': 'MouseEvent', // 'TouchEvent'
-  };
-
-
-  events.forEach((name, type) {
-    describe('ng-$name', () {
-      TestBed _;
-
-      beforeEach(beforeEachTestBed((tb) => _ = tb));
-
-      it('should evaluate the expression on $name', inject(() {
-        _.compile('<button ng-$name="abc = true; event = \$event"></button>');
-        _.triggerEvent(_.rootElement, name, type);
-        expect(_.rootScope['abc']).toEqual(true);
-        expect(_.rootScope['event'] is dom.UIEvent).toEqual(true);
-      }));
-    });
-  });
+    addTest('touchcancel', 'MouseEvent' /*TouchEvent*/);
+    addTest('touchend', 'MouseEvent' /*TouchEvent*/);
+    addTest('touchmove', 'MouseEvent' /*TouchEvent*/);
+    addTest('touchstart', 'MouseEvent' /*TouchEvent*/);
 }
