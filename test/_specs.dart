f7603e6fe3ddfa606a9d0a5177754d83a48a1feb
James deBoer <james@huronbox.com>
2013-09-18 17:00:18 -0700

    feat(jquery): Added a .css method

--- test/_specs.dart
+++ test/_specs.dart
@@ -179,6 +179,9 @@ class JQuery implements List<Node> {
   textWithShadow() => fold('', (t, n) => '${t}${renderedText(n)}');
   find(selector) => fold(new JQuery(), (jq, n) => jq..addAll(n.queryAll(selector)));
   hasClass(String name) => fold(false, (hasClass, node) => hasClass ? true : node.classes.contains(name));
+  css(String name, [String value]) => accessor(
+          (Element n) => n.style.getPropertyValue(name),
+          (Element n, v) => n.style.setProperty(name, value), value);
 }
 
 class Logger implements List {
