491fae3300d0a8a64ea1d3c3b67c8a1af5dbd7b4
Andrew Kiellor <akiellor@gmail.com>
2013-09-25 12:36:45 -0700

    chore(tests): Make analyse.sh platform agnostic.

--- analyze.sh
+++ analyze.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+DART_ANALYSER_BIN=`which dartanalyzer`
+
 # OS-specific Dartium path defaults
-case $( uname -s ) in
-  Darwin)
-    DART_SDK=/Applications/dart/dart-sdk
-esac
-
-
+if [ -z $DART_ANALYSER_BIN ]; then
+  case $( uname -s ) in
+    Darwin)
+      DART_ANALYSER_BIN=/Applications/dart/dart-sdk/bin/dartanalyzer
+    ;;
+    Linux)
+      DART_ANALYSER_BIN=/opt/dart-sdk/bin/dartanalyzer
+  esac
+fi
 
 OUT=tmp/all.dart
 mkdir -p tmp
@@ -18,4 +23,4 @@ do
   echo import \'../$FILE\'\; >> $OUT
 done
 
-./generate.sh && $DART_SDK/bin/dartanalyzer $OUT
+./generate.sh && $DART_ANALYSER_BIN $OUT
