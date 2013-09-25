f52065fa212beef34fd33eec9f8ad06ee99102ac
Misko Hevery <misko@hevery.com>
2013-09-24 21:40:07 -0700

    chore(import): correct missing directive.dart

--- lib/angular.dart
+++ lib/angular.dart
@@ -1,14 +1,14 @@
 library angular;
 
-import 'bootstrap.dart';              export 'bootstrap.dart';
-import 'cache.dart';                  export 'cache.dart';
-import 'directive.dart';              export 'directive.dart';
-import 'exception_handler.dart';      export 'exception_handler.dart';
-import 'interpolate.dart';            export 'interpolate.dart';
-import 'scope.dart';                  export 'scope.dart';
-import 'zone.dart';                   export 'zone.dart';
+export 'bootstrap.dart';
+export 'cache.dart';
+export 'exception_handler.dart';
+export 'interpolate.dart';
+export 'scope.dart';
+export 'zone.dart';
 
-import 'parser/parser_library.dart';  export 'parser/parser_library.dart';
-import 'dom/all.dart';                export 'dom/all.dart';
-import 'dom/http.dart';               export 'dom/http.dart';
-import 'directives/all.dart';         export 'directives/all.dart';
+export 'parser/parser_library.dart';
+export 'dom/directive.dart';
+export 'dom/all.dart';
+export 'dom/http.dart';
+export 'directives/all.dart';
--- lib/dom/all.dart
+++ lib/dom/all.dart
@@ -1,7 +1,7 @@
 library angular.dom.all;
 
-import 'block.dart';                      export 'block.dart';
-import 'block_factory.dart';              export 'block_factory.dart';
-import 'compiler.dart';                   export 'compiler.dart';
-import 'tree_sanitizer.dart';             export 'tree_sanitizer.dart';
+export 'block.dart';
+export 'block_factory.dart';
+export 'compiler.dart';
+export 'tree_sanitizer.dart';
 
