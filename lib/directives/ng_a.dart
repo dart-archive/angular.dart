4520d9350146a7387ecb14cad2aa8c96dd37b687
Andrew Kiellor <akiellor@gmail.com>
2013-09-26 15:35:25 -0700

    Added docs for anchor directive.

--- lib/directives/ng_a.dart
+++ lib/directives/ng_a.dart
@@ -3,6 +3,19 @@ library angular.directive.ng_a;
 import 'dart:html' as dom;
 import '../dom/directive.dart';
 
+/**
+ * @ngdoc directive
+ * @name ng.directive:a
+ * @restrict E
+ *
+ * @description
+ * Modifies the default behavior of the html A tag so that the default action is prevented when
+ * the href attribute is empty.
+ *
+ * This change permits the easy creation of action links with the `ngClick` directive
+ * without changing the location or causing page reloads, e.g.:
+ * `<a href="" ng-click="model.$save()">Save</a>`
+ */
 @NgDirective(selector: 'a[href]')
 class NgAAttrDirective {
   dom.Element element;
