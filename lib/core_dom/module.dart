d28976f5d445a40eeb529ebd9a34e91bb0705252
swizard <swizard@gmail.com>
2013-10-30 00:01:40 +0900

    fix(json) change dart json to package json

--- lib/core_dom/module.dart
+++ lib/core_dom/module.dart
@@ -1,7 +1,7 @@
 library angular.core.dom;
 
 import 'dart:async' as async;
-import 'dart:json' as json;
+import 'package:json/json.dart' as json;
 import 'dart:html' as dom;
 import 'dart:mirrors';
 
