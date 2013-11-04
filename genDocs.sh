778fd84a65dc48fadf47970ca578b85f1d226dc9
Misko Hevery <misko@hevery.com>
2013-11-04 15:25:01 -0800

    chore(docs): move docs to doc

--- genDocs.sh
+++ genDocs.sh
@@ -2,6 +2,7 @@
 
 dartdoc \
     --package-root=packages/ \
+    --out doc \
     --mode=static \
     --exclude-lib=js,metadata,meta,mirrors,intl,number_symbols,number_symbol_data,intl_helpers,date_format_internal,date_symbols,angular.util \
     lib/angular.dart lib/mock/module.dart \
