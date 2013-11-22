283ea257309f8c91f80942c894ec4193b60fd725
Misko Hevery <misko@hevery.com>
2013-11-21 21:50:41 -0800

    feat(interpolate): use $watchSet to remove memory pressure

--- lib/core/interpolate.dart
+++ lib/core/interpolate.dart
@@ -5,6 +5,26 @@ String _endSymbol = '}}';
 num _startSymbolLength = _startSymbol.length;
 num _endSymbolLength = _endSymbol.length;
 
+class Interpolation {
+  final String template;
+  final List<String> seperators;
+  final List<Expression> watchExpressions;
+  Function setter = (_) => _;
+
+  Interpolation(this.template, this.seperators, this.watchExpressions);
+
+  String call(List parts, [_, __]) {
+    var str = [];
+    for(var i = 0, ii = parts.length; i < ii; i++) {
+      str.add(seperators[i]);
+      var value = parts[i];
+      str.add(value == null ? '' : '$value');
+    }
+    str.add(seperators.last);
+    return setter(str.join(''));
+  }
+}
+
 /**
  * Compiles a string with markup into an interpolation function. This service
  * is used by the HTML [Compiler] service for data binding.
@@ -15,10 +35,9 @@ num _endSymbolLength = _endSymbol.length;
  *     expect(exp({name:'Angular'}).toEqual('Hello Angular!');
  */
 class Interpolate {
-  Parser _parse;
-  ExceptionHandler _exceptionHandler;
+  final Parser _parse;
 
-  Interpolate(Parser this._parse, ExceptionHandler this._exceptionHandler);
+  Interpolate(Parser this._parse);
 
   /**
    * Compile markup text into interpolation function.
@@ -29,67 +48,35 @@ class Interpolate {
    *      Strings with no embedded expression will return null for the
    *      interpolation function.
    */
-  Expression call(String text, [bool mustHaveExpression = false]) {
+  Interpolation call(String template, [bool mustHaveExpression = false]) {
     num startIndex;
     num endIndex;
     num index = 0;
-    List chunks = [];
-    num length = text.length;
+    num length = template.length;
     bool hasInterpolation = false;
     String exp;
-    List concat = [];
-    Expression fn;
+    List<String> separators = [];
+    List<Expression> watchExpressions = [];
 
     while(index < length) {
-      if ( ((startIndex = text.indexOf(_startSymbol, index)) != -1) &&
-           ((endIndex = text.indexOf(_endSymbol, startIndex + _startSymbolLength)) != -1) ) {
-        if (index != startIndex) {
-          chunks.add(text.substring(index, startIndex));
-        }
-        fn = _parse(exp = text.substring(startIndex + _startSymbolLength, endIndex));
-        chunks.add(fn);
-        fn.exp = exp;
+      if ( ((startIndex = template.indexOf(_startSymbol, index)) != -1) &&
+           ((endIndex = template.indexOf(_endSymbol, startIndex + _startSymbolLength)) != -1) ) {
+        separators.add(template.substring(index, startIndex));
+        exp = template.substring(startIndex + _startSymbolLength, endIndex);
+        watchExpressions.add((_parse(exp)..exp = exp).eval);
         index = endIndex + _endSymbolLength;
         hasInterpolation = true;
       } else {
         // we did not find anything, so we have to add the remainder to the chunks array
-        if (index != length) {
-          chunks.add(text.substring(index));
-        }
+        separators.add(template.substring(index));
         index = length;
       }
     }
-
-    if ((length = chunks.length) == 0) {
-      // we added, nothing, must have been an empty string.
-      chunks.add('');
-      length = 1;
+    if (separators.length == watchExpressions.length) {
+      separators.add('');
     }
-
     if (!mustHaveExpression  || hasInterpolation) {
-      fn = new Expression((context, [locals]) {
-        try {
-          for(var i = 0, ii = length, chunk; i<ii; i++) {
-            if ((chunk = chunks[i]) is Expression) {
-              chunk = chunk.eval(context);
-              if (chunk == null) {
-                chunk = '';
-              } else if (!(chunk is String)) {
-                chunk = '$chunk';
-              }
-            }
-            concat.add(chunk);
-          }
-          return concat.join('');
-        } catch(err, s) {
-          _exceptionHandler("\$interpolate error! Can't interpolate: $text\n$err", s);
-        } finally {
-          concat.length = 0;
-        }
-      });
-      fn.exp = text;
-      fn.parts = chunks;
-      return fn;
+      return new Interpolation(template, separators, watchExpressions);
     }
   }
 }
