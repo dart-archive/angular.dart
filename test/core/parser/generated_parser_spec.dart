fe4018e21679cf3a4989d0556d90ab460f8eb688
James deBoer <james@huronbox.com>
2013-11-25 18:40:14 -0800

    feat(spec): Do not fall-thru to the dynamic parser in the static parser spec

--- test/core/parser/generated_parser_spec.dart
+++ test/core/parser/generated_parser_spec.dart
@@ -4,10 +4,18 @@ import '../../_specs.dart';
 import 'parser_spec.dart' as parser_spec;
 import 'generated_functions.dart' as generated_functions;
 
+class AlwaysThrowError implements DynamicParser {
+  call(String x) { throw "Fall-thru to DynamicParser disabled [$x]"; }
+  primaryFromToken(Token token, parserError) => null;
+}
+
+
 main() {
   describe('generated parser', () {
     beforeEach(module((Module module) {
       module.type(Parser, implementedBy: StaticParser);
+      module.type(DynamicParser, implementedBy: AlwaysThrowError);
+
       module.factory(StaticParserFunctions, (Injector injector) {
         return generated_functions.functions(injector.get(FilterMap));
       });
