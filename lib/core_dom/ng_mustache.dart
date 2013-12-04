0cef6af7cc9d2d2f4fa5269194e8d820cadaa42b
Justin Fagnani <justin@fagnani.com>
2013-12-03 20:30:39 -0800

    Make NgMustacheDirective use node binding

--- lib/core_dom/ng_mustache.dart
+++ lib/core_dom/ng_mustache.dart
@@ -22,15 +22,26 @@ class NgTextMustacheDirective {
 @NgDirective(selector: r'[*=/{{.*}}/]')
 class NgAttrMustacheDirective {
   static RegExp ATTR_NAME_VALUE_REGEXP = new RegExp(r'^([^=]+)=(.*)$');
+  static RegExp EXPR_REGEXP = new RegExp(r'^{{(.*)}}$');
 
 // This Directive is special and does not go through injection.
-  NgAttrMustacheDirective(NodeAttrs attrs, String markup, Interpolate interpolate, Scope scope) {
+  NgAttrMustacheDirective(dom.Node node, NodeAttrs attrs, String markup, Interpolate interpolate, Scope scope, Parser parser) {
     var match = ATTR_NAME_VALUE_REGEXP.firstMatch(markup);
     var attrName = match[1];
-    Interpolation interpolation = interpolate(match[2]);
-    interpolation.setter = (text) => attrs[attrName] = text;
-    interpolation.setter('');
-    scope.$watchSet(interpolation.watchExpressions, interpolation.call, markup.trim());
+    var exprMatch = EXPR_REGEXP.firstMatch(match[2]);
+
+    var box = new ObservableBox();
+    var binding = nodeBind(node).bind(attrName, box, 'value');
+
+    if (exprMatch != null) {
+      Expression expression = parser(EXPR_REGEXP.firstMatch(match[2])[1]);
+      box.changes.listen((_) => expression.assign(scope, box.value));
+      scope.$watch(expression.eval, (newValue, oldValue) => box.value = newValue, markup.trim());
+    } else {
+      Interpolation interpolation = interpolate(match[2]);
+      interpolation.setter = (text) => box.value = text;
+      interpolation.setter('');
+      scope.$watchSet(interpolation.watchExpressions, interpolation.call, markup.trim());
+    }
   }
 }
-
