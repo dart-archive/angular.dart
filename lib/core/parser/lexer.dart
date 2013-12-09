18c218f62c8853fdbf2c726f68c59b30fbe9af17
Kasper Lund <kasperl@google.com>
2013-12-09 12:01:23 +0100

    Get rid of the old lexer.

--- lib/core/parser/lexer.dart
+++ lib/core/parser/lexer.dart
@@ -1,12 +1,7 @@
 part of angular.core.parser;
 
 @NgInjectableService()
-abstract class Lexer {
-  factory Lexer() => new _NewLexer();
-  List<Token> call(String text);
-}
-
-class _NewLexer implements Lexer {
+class Lexer {
   List<Token> call(String text) {
     Scanner scanner = new Scanner(text);
     List<Token> tokens = [];
@@ -201,12 +196,12 @@ class Scanner {
     advance();  // Skip initial quote.
 
     StringBuffer buffer;
-    int last = index;
+    int marker = index;
 
     while (peek != quote) {
       if (peek == $BACKSLASH) {
         if (buffer == null) buffer = new StringBuffer();
-        if (last < index) buffer.write(input.substring(last, index));
+        buffer.write(input.substring(marker, index));
         advance();
         int unescaped;
         if (peek == $u) {
@@ -221,7 +216,7 @@ class Scanner {
           advance();
         }
         buffer.writeCharCode(unescaped);
-        last = index;
+        marker = index;
       } else if (peek == $EOF) {
         error('Unterminated quote');
       } else {
@@ -229,14 +224,14 @@ class Scanner {
       }
     }
 
+    String last = input.substring(marker, index);
     advance();  // Skip terminating quote.
-
     String string = input.substring(start, index);
-    String unescaped;
-    if (buffer == null) {
-      unescaped = input.substring(start + 1, index - 1);
-    } else {
-      if (last < index - 1) buffer.write(input.substring(last, index - 1));
+
+    // Compute the unescaped string value.
+    String unescaped = last;
+    if (buffer != null) {
+      buffer.write(last);
       unescaped = buffer.toString();
     }
     return new Token(start, string)..withValue(unescaped);
@@ -252,237 +247,5 @@ class Scanner {
     // have been updated.
     int position = index + offset;
     throw "Lexer Error: $message at column $position in expression [$input]";
-  }
-}
-
-class _OldLexer implements Lexer {
-  static const String QUOTES = "\"'";
-  static const String DOT = ".";
-  static const String SPECIAL = "(){}[].,;:";
-  static const String JSON_SEP = "{,";
-  static const String JSON_OPEN = "{[";
-  static const String JSON_CLOSE = "}]";
-  static const String WHITESPACE = " \r\t\n\v\u00A0";
-  static const String EXP_OP = "Ee";
-  static const String SIGN_OP = "+-";
-
-  static Map<String, String> ESCAPE =
-      {"n":"\n", "f":"\f", "r":"\r", "t":"\t", "v":"\v", "'":"'", '"':'"'};
-
-  List<Token> call(String text) {
-    List<Token> tokens = [];
-    Token token;
-    int index = 0;
-    int lastIndex;
-    int textLength = text.length;
-    String ch;
-    String lastCh = ":";
-
-    isIn(String charSet, [String c]) =>
-      charSet.indexOf(c != null ? c : ch) != -1;
-    was(String charSet) => charSet.indexOf(lastCh) != -1;
-
-    cc(String s) => s.codeUnitAt(0);
-
-    bool isNumber([String c]) {
-      int cch = cc(c != null ? c : ch);
-      return cc('0') <= cch && cch <= cc('9');
-    }
-
-    isIdent() {
-      int cch = cc(ch);
-      return
-        cc('a') <= cch && cch <= cc('z') ||
-        cc('A') <= cch && cch <= cc('Z') ||
-        cc('_') == cch || cch == cc('\$');
-    }
-
-    isWhitespace([String c]) => isIn(WHITESPACE, c);
-    isExpOperator([String c]) => isIn(SIGN_OP, c) || isNumber(c);
-
-    String peek() => index + 1 < textLength ? text[index + 1] : "EOF";
-
-    lexError(String s) { throw "Lexer Error: $s at column $index in expression [$text]"; }
-
-    // whileChars takes two functions: One called for each character
-    // and a second, optional function call at the end of the file.
-    // If the first function returns false, the the loop stops and endFn
-    // is not run.
-    whileChars(fn(), [endFn()]) {
-      while (index < textLength) {
-        ch = text[index];
-        int lastIndex = index;
-        if (fn() == false) {
-          return;
-        }
-        if (lastIndex >= index) {
-          throw "while chars loop must advance at index $index";
-        }
-      }
-      if (endFn != null) { endFn(); }
-    }
-
-    readString() {
-      int start = index;
-
-      String string = "";
-      String rawString = ch;
-      String quote = ch;
-
-      index++;
-
-      whileChars(() {
-        rawString += ch;
-        if (ch == '\\') {
-          index++;
-          whileChars(() {
-            rawString += ch;
-            if (ch == 'u') {
-              String hex = text.substring(index + 1, index + 5);
-              int charCode = int.parse(hex, radix: 16,
-              onError: (s) { lexError('Invalid unicode escape [\\u$hex]'); });
-              string += new String.fromCharCode(charCode);
-              index += 5;
-            } else {
-              var rep = ESCAPE[ch];
-              if (rep != null) {
-                string += rep;
-              } else {
-                string += ch;
-              }
-              index++;
-            }
-            return false; // BREAK
-          });
-        } else if (ch == quote) {
-          index++;
-          tokens.add(new Token(start, rawString)
-          ..withValue(string));
-          return false; // BREAK
-        } else {
-          string += ch;
-          index++;
-        }
-      }, () {
-        lexError('Unterminated quote starting at $start');
-      });
-    }
-
-    readNumber() {
-      String number = "";
-      int start = index;
-      bool simpleInt = true;
-      whileChars(() {
-        if (ch == '.') {
-          number += ch;
-          simpleInt = false;
-        } else if (isNumber()) {
-          number += ch;
-        } else {
-          String peekCh = peek();
-          if (isIn(EXP_OP) && isExpOperator(peekCh)) {
-            simpleInt = false;
-            number += ch;
-          } else if (isExpOperator() && peekCh != '' && isNumber(peekCh) && isIn(EXP_OP, number[number.length - 1])) {
-            simpleInt = false;
-            number += ch;
-          } else if (isExpOperator() && (peekCh == '' || !isNumber(peekCh)) &&
-          isIn(EXP_OP, number[number.length - 1])) {
-            lexError('Invalid exponent');
-          } else {
-            return false; // BREAK
-          }
-        }
-        index++;
-      });
-      var ret = simpleInt ? int.parse(number) : double.parse(number);
-      tokens.add(new Token(start, number)..withValue(ret));
-    }
-
-    readIdent() {
-      String ident = "";
-      int start = index;
-      int lastDot = -1, peekIndex = -1;
-      String methodName;
-
-
-      whileChars(() {
-        if (ch == '.' || isIdent() || isNumber()) {
-          if (ch == '.') {
-            lastDot = index;
-          }
-          ident += ch;
-        } else {
-          return false; // BREAK
-        }
-        index++;
-      });
-
-      // The identifier had a . in the identifier
-      if (lastDot != -1) {
-        peekIndex = index;
-        while (peekIndex < textLength) {
-          String peekChar = text[peekIndex];
-          if (peekChar == "(") {
-            methodName = ident.substring(lastDot - start + 1);
-            ident = ident.substring(0, lastDot - start);
-            index = peekIndex;
-          }
-          if (isWhitespace(peekChar)) {
-            peekIndex++;
-          } else {
-            break;
-          }
-        }
-      }
-
-      var token = new Token(start, ident);
-
-      if (OPERATORS.containsKey(ident)) {
-        token.withOp(ident);
-      } else {
-        token.withGetterSetter(ident);
-      }
-
-      tokens.add(token);
-
-      if (methodName != null) {
-        tokens.add(new Token(lastDot, '.'));
-        tokens.add(new Token(lastDot + 1, methodName));
-      }
-    }
-
-    oneLexLoop() {
-      if (isIn(QUOTES)) {
-        readString();
-      } else if (isNumber() || isIn(DOT) && isNumber(peek())) {
-        readNumber();
-      } else if (isIdent()) {
-        readIdent();
-      } else if (isIn(SPECIAL)) {
-        tokens.add(new Token(index, ch));
-        index++;
-      } else if (isWhitespace()) {
-        index++;
-      } else {
-        // Check for two character operators (e.g. "==")
-        String ch2 = ch + peek();
-
-        if (OPERATORS.containsKey(ch2)) {
-          tokens.add(new Token(index, ch2)..withOp(ch2));
-          index += 2;
-        } else if (OPERATORS.containsKey(ch)) {
-          tokens.add(new Token(index, ch)..withOp(ch));
-          index++;
-        } else {
-          lexError('Unexpected next character [$ch]');
-        }
-      }
-    }
-
-    whileChars(() {
-      oneLexLoop();
-    });
-    return tokens;
   }
 }
