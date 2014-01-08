c8939613fbf8287e250e3982704cbac783daf9dd
Misko Hevery <misko@hevery.com>
2014-01-08 13:29:18 -0800

    chore(travis): deleted unneeded script

--- scripts/travis/chrome-no-sandbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# see
-#   - https://code.google.com/p/chromium/issues/detail?id=138505
-#   - https://code.google.com/p/chromium/wiki/LinuxSUIDSandbox
-
-
-/usr/bin/google-chrome --no-sandbox $@
