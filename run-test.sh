4cd0e22bc76c1426a3baae868f26c63cc36ba118
Victor Berchet <victor@suumit.com>
2014-01-25 11:17:28 +0100

    Update run-test.sh
    analyze.sh is now located in "/scripts", the former version is deprecated.

--- run-test.sh
+++ run-test.sh
@@ -35,7 +35,7 @@ dart --checked test/io/all.dart
 # run expression extractor tests
 scripts/test-expression-extractor.sh
 
-./analyze.sh &&
+./scripts/analyze.sh &&
   node_modules/jasmine-node/bin/jasmine-node playback_middleware/spec/ &&
   node "node_modules/karma/bin/karma" start karma.conf \
     --reporters=junit,dots --port=8765 --runner-port=8766 \
