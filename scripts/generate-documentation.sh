a96a3ca93b6cf2542b3bbd7eec7cd1cd5c3ae65d
Patrice Chalin <pchalin@gmail.com>
2014-02-11 19:47:16 -0600

    Remove --mode option for `docgen`

--- scripts/generate-documentation.sh
+++ scripts/generate-documentation.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 . $(dirname $0)/env.sh
 
-# Temporary during transition period from dartdoc to docgen.
-
-if [ ! -x "$DART_DOCGEN" ]; then
-    # Fall back on old dartdoc
-    DART_DOCGEN=$DARTDOC
+# Temporary during transition period from use of dartdoc to docgen.
+if [ -x "$DART_DOCGEN" ]; then
+    DOC_CMD="$DART_DOCGEN"
+    MODE_OPTION=
+else
+    DOC_CMD="$DARTDOC"
+    MODE_OPTION="--mode=static"
 fi
 
-$DART_DOCGEN \
+echo Generating documentation using $DOC_CMD
+$DOC_CMD \
     --package-root=packages/ \
     --out doc \
-    --mode=static \
+    $MODE_OPTION \ 
     --exclude-lib=js,metadata,meta,mirrors,intl,number_symbols,number_symbol_data,intl_helpers,date_format_internal,date_symbols,angular.util \
-    packages/angular/angular.dart lib/mock/module.dart \
+    packages/angular/angular.dart lib/mock/module.dart
+
