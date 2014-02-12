b6fbc9671cd2d1145175a024de683eeea7010e69
Patrice Chalin <pchalin@gmail.com>
2014-02-11 21:01:52 -0600

    Fix inclusion of MODE_OPTION

--- scripts/generate-documentation.sh
+++ scripts/generate-documentation.sh
@@ -2,9 +2,9 @@
 . $(dirname $0)/env.sh
 
 echo DART_DOCGEN=$DART_DOCGEN
-ls -l $DART_DOCGEN
+ls -l "$DART_DOCGEN" || true
 echo DARTDOC=$DARTDOC
-ls -l $DARTDOC
+ls -l "$DARTDOC" || true
 
 # Temporary during transition period from use of dartdoc to docgen.
 if [ -x "$DART_DOCGEN" ]; then
@@ -15,15 +15,15 @@ elif [ -x "$DARTDOC" ]; then
     MODE_OPTION="--mode=static"
 else
     echo "There is no tool to generate the documentation!"
-    echo "Failing silently during this transition period from dartdoc to docgen."
+    echo "Report success during this transition period from dartdoc to docgen."
     exit 0;
 fi
 
-echo Generating documentation using $DOC_CMD
-$DOC_CMD \
+echo Generating documentation using $DOC_CMD using mode option '$MODE_OPTION'
+"$DOC_CMD" $MODE_OPTION \
     --package-root=packages/ \
     --out doc \
-    $MODE_OPTION \ 
     --exclude-lib=js,metadata,meta,mirrors,intl,number_symbols,number_symbol_data,intl_helpers,date_format_internal,date_symbols,angular.util \
     packages/angular/angular.dart lib/mock/module.dart
+
 
