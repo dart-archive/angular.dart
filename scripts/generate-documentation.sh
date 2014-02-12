dae87b924e46d2951b19540dfe2d5dda469d7441
Patrice Chalin <pchalin@gmail.com>
2014-02-11 19:55:09 -0600

    Do not generate doc if there is no doc tool

--- scripts/generate-documentation.sh
+++ scripts/generate-documentation.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 . $(dirname $0)/env.sh
 
+echo DART_DOCGEN=$DART_DOCGEN
+ls -l $DART_DOCGEN
+echo DARTDOC=$DARTDOC
+ls -l $DARTDOC
+
 # Temporary during transition period from use of dartdoc to docgen.
 if [ -x "$DART_DOCGEN" ]; then
     DOC_CMD="$DART_DOCGEN"
     MODE_OPTION=
-else
+elif [ -x "$DARTDOC" ]; then
     DOC_CMD="$DARTDOC"
     MODE_OPTION="--mode=static"
+else
+    echo "There is no tool to generate the documentation!"
+    echo "Failing silently during this transition period from dartdoc to docgen."
+    exit 0;
 fi
 
 echo Generating documentation using $DOC_CMD
