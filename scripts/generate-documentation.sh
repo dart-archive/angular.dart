9a1ef31d66f151f22b79893e11251a6780605257
Misko Hevery <misko@hevery.com>
2014-02-12 21:29:13 -0800

    fix(doc-gen): dartbug.com/16752

--- scripts/generate-documentation.sh
+++ scripts/generate-documentation.sh
@@ -3,16 +3,8 @@
 
 # Temporary during transition period from use of dartdoc to docgen.
 if [ -x "$DART_DOCGEN" ]; then
-    # docgen seems to freeze when it processes the angular.dart files
-    # https://code.google.com/p/dart/issues/detail?id=16752
-    # so disable it for now
-    # DOC_CMD="$DART_DOCGEN"
-    # MODE_OPTION=
-    echo "DISABLING DOCUMENT GENERATION due to isses with docgen."
-    echo "https://code.google.com/p/dart/issues/detail?id=16752"
-    echo "----"
-    echo "Reporting success none-the-less during this docgen beta period."
-    exit 0;
+    DOC_CMD="$DART_DOCGEN"
+    MODE_OPTION=
 elif [ -x "$DARTDOC" ]; then
     DOC_CMD="$DARTDOC"
     MODE_OPTION="--mode=static"
