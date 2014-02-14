d0e25b720a04824eb0d37520d58830eecc0d190d
Kasper Lund <kasper@verdich.dk>
2014-02-14 13:24:20 +0100

    chore(style) clean up warnings
    Closes $564

--- test/core/parser/generated_functions.dart
+++ test/core/parser/generated_functions.dart
@@ -3,6 +3,4 @@ library angular.service.parser.generated_expressions_template;
 import 'package:angular/core/parser/parser.dart' show StaticParserFunctions;
 import 'package:angular/core/parser/utils.dart';
 
-typedef Function FilterLookup(String filterName);
-
-functions(FilterLookup filterLookup) { throw "This should never be called"; } // REMOVE
+functions() { throw "This should never be called"; } // REMOVE
--- test/io/test_files/cssUrls/main.dart
+++ test/io/test_files/cssUrls/main.dart
@@ -1,7 +1,6 @@
 library test_files.main;
 
 import 'package:angular/core/module.dart';
-import 'package:angular/tools/template_cache_annotation.dart';
 
 @NgComponent(
     selector: 'my-component',
