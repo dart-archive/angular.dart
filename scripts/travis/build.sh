f203b92972d1d73b76a7784436187bce9f12b781
Victor <victor@suumit.com>
2014-02-25 12:46:44 +0100

    chore(karma): remove unsupported --no-color option
    Use --no-colors instead
    
    Closes #613

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -14,7 +14,7 @@ dart -c test/io/all.dart
 ./node_modules/jasmine-node/bin/jasmine-node playback_middleware/spec/ &&
   node "node_modules/karma/bin/karma" start karma.conf \
     --reporters=junit,dots --port=8765 --runner-port=8766 \
-    --browsers=Dartium,ChromeNoSandbox --single-run --no-colors --no-color
+    --browsers=Dartium,ChromeNoSandbox --single-run --no-colors
 
 ./scripts/generate-documentation.sh
 
