49705f179c99db219d437d40a8a04fd0fe4b9ce9
Matias Niemelä <matias@yearofmoo.com>
2014-03-11 00:23:36 -0400

    feat(core_dom): introduce NgElement

---
+++ lib/core_dom/ng_element.dart
@@ -0,0 +1,38 @@
+part of angular.core.dom;
+
+class NgElement {
+
+  final dom.Element node;
+  final Scope _scope;
+  final NgAnimate _animate;
+  final _classes = new Map<String, bool>();
+
+  NgElement(this.node, this._scope, NgAnimate this._animate);
+
+  addClass(String className) {
+    if(_classes.isEmpty) {
+      _listenOnWrite();
+    }
+    _classes[className] = true;
+  }
+
+  removeClass(String className) {
+    if(_classes.isEmpty) {
+      _listenOnWrite();
+    }
+    _classes[className] = false;
+  }
+
+  _listenOnWrite() {
+    _scope.rootScope.domWrite(() => flush());
+  }
+
+  flush() {
+    _classes.forEach((className, status) {
+      status == true
+        ? _animate.addClass(node, className)
+        : _animate.removeClass(node, className);
+    });
+    _classes.clear();
+  }
+}
