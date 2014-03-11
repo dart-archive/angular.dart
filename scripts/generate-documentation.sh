59e4808a87f1c8a7160e4976de0f303cfc75786f
naomi black <naomitraveller+nospam@gmail.com>
2014-03-11 16:07:57 -0700

    chore: move the README edit earlier and improve git fetch of docviewer
    Closes #707

--- scripts/generate-documentation.sh
+++ scripts/generate-documentation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 . $(dirname $0)/env.sh
 
+# Temporary change to delete the Build Status image markdown from the README (image md not supported by dartdoc-viewer)
+cp README.md README-orig.md
+cat README-orig.md | sed "1s/^AngularDart.*/AngularDart/" > README.md
 
 # Dart doc can not be run from the same directory as dartdoc-viewer
 # see: https://code.google.com/p/dart/issues/detail?id=17231
@@ -39,10 +42,18 @@ cd ..
 
 DOCVIEWER_DIR="dartdoc-viewer";
 if [ ! -d "$DOCVIEWER_DIR" ]; then
-  git clone https://github.com/angular/dartdoc-viewer.git -b angular-skin $DOCVIEWER_DIR
+   git clone https://github.com/angular/dartdoc-viewer.git -b angular-skin $DOCVIEWER_DIR
+else
+   (cd $DOCVIEWER_DIR; git pull origin angular-skin)
 fi;
 
+# Create a version file from the current build version
 head CHANGELOG.md | awk 'NR==2' | sed 's/^# //' > docs/VERSION
+
 rm -rf $DOCVIEWER_DIR/client/web/docs/
 mv docs/ $DOCVIEWER_DIR/client/web/docs/
 (cd $DOCVIEWER_DIR/client; pub build)
+
+# Revert the temp copy of the README.md file
+ mv README-orig.md README.md
+
