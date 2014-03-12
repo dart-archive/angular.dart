7c5bdb01dae7933540679e070ace5dc9fe223cd2
James deBoer <james@huronbox.com>
2014-03-12 11:26:21 -0700

    feat(travis): Seperate Chrome and Dartium into two different jobs.

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -3,18 +3,29 @@
 set -evx
 . ./scripts/env.sh
 
-# run io tests
-dart -c test/io/all.dart
+# skip auxiliary tests if we are only running dart2js
+if [[ $TESTS != "dart2js" ]]; then
+  # run io tests
+  dart -c test/io/all.dart
 
-./scripts/generate-expressions.sh
-./scripts/analyze.sh
+  ./scripts/generate-expressions.sh
+  ./scripts/analyze.sh
 
-./node_modules/jasmine-node/bin/jasmine-node ./scripts/changelog/
+  ./node_modules/jasmine-node/bin/jasmine-node ./scripts/changelog/;
+fi
+
+BROWSERS=Dartium,ChromeNoSandbox
+if [[ $TESTS == "dart2js" ]]; then
+  BROWSERS=ChromeNoSandbox;
+elif [[ $TESTS == "vm" ]]; then
+  BROWSERS=Dartium;
+fi
 
 ./node_modules/jasmine-node/bin/jasmine-node playback_middleware/spec/ &&
   node "node_modules/karma/bin/karma" start karma.conf \
     --reporters=junit,dots --port=8765 --runner-port=8766 \
-    --browsers=Dartium,ChromeNoSandbox --single-run --no-colors
+    --browsers=$BROWSERS --single-run --no-colors
 
-./scripts/generate-documentation.sh
-
+if [[ $TESTS != "dart2js" ]]; then
+  ./scripts/generate-documentation.sh;
+fi
