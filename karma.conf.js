9a92b5cc0c2166cf96083b231d249f51dfd446ff
James deBoer <james@huronbox.com>
2014-03-13 14:45:38 -0700

    chore(karma): Increase the dart2js timeout

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,8 @@ module.exports = function(config) {
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 20000,
-    // 3 minutes is enough time for dart2js to run
-    browserNoActivityTimeout: 180000,
+    // 5 minutes is enough time for dart2js to run on Travis...
+    browserNoActivityTimeout: 300000,
 
     plugins: [
       'karma-dart',
