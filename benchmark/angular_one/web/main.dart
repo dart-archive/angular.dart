d6ef28d1e65db8629c6bfc17d7fbb20d8071704e
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-03-13 22:47:06 +1100

    Add CreateModuleBenchmark and InstantiateDomBenchmark

--- benchmark/angular_one/web/main.dart
+++ benchmark/angular_one/web/main.dart
@@ -1,14 +1,42 @@
+import 'dart:html';
 import 'package:angular/angular.dart';
-
+import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:angular_one/person.dart';
 import 'package:angular_one/controller.dart';
 
+class CreteModuleBenchmark extends BenchmarkBase {
+  var m;
+
+  CreteModuleBenchmark(): super('CreteModuleBenchmark');
+
+  void run() => m = new BenchmarkModule();
+
+  void tearDown() => m.hashCode;
+}
+
+class InstantiateDomBenchmark extends BenchmarkBase {
+  InstantiateDomBenchmark(): super('InstantiateDomBenchmark');
+
+  Compiler compiler;
+  Injector injector;
+  var rootElements;
+  var directiveMap;
+
+  void run() {
+    compiler(rootElements, directiveMap)(injector, rootElements);
+  }
+
+  void setup() {
+    injector = ngBootstrap(module: new BenchmarkModule());
+    rootElements = [document.body];
+    compiler = injector.get(Compiler);
+    directiveMap = injector.get(DirectiveMap);
+  }
+}
+
 void main() {
-  Stopwatch moduleCreationStopwatch = new Stopwatch();
-  moduleCreationStopwatch.start();
-  var m = new BenchmarkModule();
-  moduleCreationStopwatch.stop();
-  ngBootstrap(module: m);
+  new CreteModuleBenchmark()..report();
+  new InstantiateDomBenchmark()..report();
 }
 
 class BenchmarkModule extends Module {
