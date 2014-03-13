aec58c75b2f6a896a37756f438e05620c7f13f66
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-03-14 09:17:15 +1100

    Remove references to deleted controller

--- benchmark/angular_one/web/module.dart
+++ benchmark/angular_one/web/module.dart
@@ -1,11 +1,9 @@
 import 'package:angular/angular.dart';
 
 import 'package:angular_one/person.dart';
-import 'package:angular_one/controller.dart';
 
 class BenchmarkModule extends Module {
   BenchmarkModule() {
     install(new PersonModule());
-    install(new ControllerModule());
   }
 }
