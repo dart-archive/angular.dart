e1d401916b9d262104a73511a3f78ab55f08a2a8
Victor <victor@suumit.com>
2014-03-19 15:54:10 +0100

    doc(NodeAttrs): rework class comments, make namings consistent

--- lib/core_dom/directive.dart
+++ lib/core_dom/directive.dart
@@ -7,9 +7,12 @@ typedef void _AttributeChanged(String newValue);
 typedef void Mustache(bool hasObservers);
 
 /**
- * NodeAttrs is a facade for element attributes. The facade is responsible
- * for normalizing attribute names as well as allowing access to the
- * value of the directive.
+ * NodeAttrs is a facade for element attributes.
+ *
+ * This facade allows reading and writing the attribute values as well as
+ * adding observers triggered when:
+ * - The value of an attribute changes,
+ * - An element becomes observed.
  */
 class NodeAttrs {
   final dom.Element element;
@@ -30,15 +33,20 @@ class NodeAttrs {
     } else {
       element.attributes[attrName] = value;
     }
+
     if (_observers != null && _observers.containsKey(attrName)) {
       _observers[attrName].forEach((notifyFn) => notifyFn(value));
     }
   }
 
   /**
-   * Observe changes to the attribute by invoking the [notifyFn] function. On
-   * registration the [notifyFn] function gets invoked synchronize with the
-   * current value.
+   * Observes changes to the attribute by invoking the [notifyFn]
+   * function. On registration the [notifyFn] function gets invoked in order to
+   * synchronize with the current value.
+   *
+   * When an observed is registered on an attributes any existing
+   * [_observerListeners] will be called with the first parameter set to
+   * [:true:]
    */
   observe(String attrName, notifyFn(String value)) {
     if (_observers == null) _observers = <String, List<_AttributeChanged>>{};
@@ -61,6 +69,11 @@ class NodeAttrs {
 
   Iterable<String> get keys => element.attributes.keys;
 
+  /**
+   * Registers a listener to be called when the attribute [attrName] becomes
+   * observed. On registration [notifyFn] function gets invoked with [:false:]
+   * as the first argument.
+   */
   void listenObserverChanges(String attrName, Mustache notifyFn) {
     _mustacheAttrs[attrName] = new _MustacheAttr(notifyFn);
     notifyFn(false);
@@ -68,7 +81,7 @@ class NodeAttrs {
 }
 
 /**
- * TemplateLoader is an asynchronous access to ShadowRoot which is
+ * [TemplateLoader] is an asynchronous access to ShadowRoot which is
  * loaded asynchronously. It allows a Component to be notified when its
  * ShadowRoot is ready.
  */
