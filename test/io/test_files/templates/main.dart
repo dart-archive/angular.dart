8952cbdd70f32e8b920942476648b1a8f1473480
Kasper Lund <kasper@verdich.dk>
2014-04-03 16:16:17 +0200

    fix(cookies): Make sure Cookies is injectable.
    Also clean-up a few warnings and remove an unnecessary workaround
    in the todo example.
    
    Closes #856

--- test/io/test_files/templates/main.dart
+++ test/io/test_files/templates/main.dart
@@ -1,6 +1,6 @@
 library test_files.main;
 
-import 'package:angular/core/module_internal.dart';
+import 'package:angular/core/annotation.dart';
 import 'package:angular/tools/template_cache_annotation.dart';
 
 part 'partial.dart';
