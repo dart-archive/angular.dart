388a782c35c06dbadb84704e68adaf69ff540440
Misko Hevery <misko@hevery.com>
2014-04-17 15:23:08 -0700

    chore: correct commit message validator instructions

--- scripts/git/validate-commit-msg.js
+++ scripts/git/validate-commit-msg.js
@@ -6,7 +6,7 @@
  *
  * Installation:
  * >> cd <angular-repo>
- * >> ln -s scripts/git/validate-commit-msg.js .git/hooks/commit-msg
+ * >> ln -s ../../scripts/git/validate-commit-msg.js .git/hooks/commit-msg
  */
 var fs = require('fs');
 var util = require('util');
