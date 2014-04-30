c27ce965f9a03c32121998ba7c6fe37ccce15931
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-04-30 23:10:27 +1000

    feat(Scope): Use VmTurnZone.onScheduleMicrotask in Scope
    BREAKING CHANGE:
    
    Previously a micro task registered in flush phase would cause a new
    digest cycle after the current digest cycle. The new behavior
    will cause an error.
    
    Closes #984

--- lib/core/scope.dart
+++ lib/core/scope.dart
@@ -644,6 +644,7 @@ class RootScope extends Scope {
   {
     _zone.onTurnDone = apply;
     _zone.onError = (e, s, ls) => _exceptionHandler(e, s);
+    _zone.onScheduleMicrotask = runAsync;
   }
 
   RootScope get rootScope => this;
@@ -787,6 +788,9 @@ class RootScope extends Scope {
 
   // QUEUES
   void runAsync(fn()) {
+    if (_state == STATE_FLUSH || _state == STATE_FLUSH_ASSERT) {
+      throw "Scheduling microtasks not allowed in $state state.";
+    }
     var chain = new _FunctionChain(fn);
     if (_runAsyncHead == null) {
       _runAsyncHead = _runAsyncTail = chain;
