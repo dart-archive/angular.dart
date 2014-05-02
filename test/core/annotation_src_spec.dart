5c22f7a6c39bac93355900107c5007cd997c32ab
Victor <victor@suumit.com>
2014-05-02 12:40:01 +0200

    squash: drop controller

--- test/core/annotation_src_spec.dart
+++ test/core/annotation_src_spec.dart
@@ -75,24 +75,4 @@ void main() => describe('annotations', () {
       expect(variables(cloneWithNewMap(decorator, {}))).toEqual(variables(decorator));
     });
   });
-
-  describe('controller', () {
-    it('should set all fields on clone when all the fields are set', () {
-      var controller = new Controller(
-          children: 'xxx',
-          map: {},
-          selector: '',
-          module: (){},
-          visibility: Directive.LOCAL_VISIBILITY,
-          exportExpressions: [],
-          exportExpressionAttrs: []
-      );
-
-      // Check that no fields are null
-      expect(nullFields(controller)).toEqual([]);
-
-      // Check that the clone is the same as the original.
-      expect(variables(cloneWithNewMap(controller, {}))).toEqual(variables(controller));
-    });
-  });
 });
