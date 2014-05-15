02ef344d0a6e1c88ffdab95af579cc2795247d92
naomi black <naomitraveller+nospam@gmail.com>
2014-05-14 21:12:59 -0700

    docs(ng-bind): Edits for dartdoc format

--- lib/directive/ng_bind.dart
+++ lib/directive/ng_bind.dart
@@ -1,19 +1,18 @@
 part of angular.directive;
 
 /**
- * The ngBind attribute tells Angular to replace the text content of the
- * specified HTML element with the value of a given expression, and to update
- * the text content when the value of that expression changes.
+ * Replaces the text content of the specified HTML element with the value of a given expression,
+ * and updates the text content when the value of that expression changes.
  *
  * Typically, you don't use ngBind directly, but instead you use the double
- * curly markup like {{ expression }} which is similar but less verbose.
+ * curly markup like `{{ expression }}` which is similar but less verbose.
  *
- * It is preferrable to use ngBind instead of {{ expression }} when a template
+ * It is preferrable to use `ng-bind` instead of `{{ expression }}` when a template
  * is momentarily displayed by the browser in its raw state before Angular
- * compiles it. Since ngBind is an element attribute, it makes the bindings
+ * compiles it. Since `ng-bind` is an element attribute, it makes the bindings
  * invisible to the user while the page is loading.
  *
- * An alternative solution to this problem would be using the ngCloak directive.
+ * An alternative solution to this problem would be using the [ngCloak] directive.
  */
 @Decorator(
   selector: '[ng-bind]',
