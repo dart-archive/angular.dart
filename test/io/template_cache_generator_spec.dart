247a30ecf41daa85048e42a1b3274a655c3de60a
Roman Kamyk <rkj@google.com>
2014-05-16 15:11:37 -0700

    test: add test for template_cache_generator css rewriting
    It use simple sed script to showcase integration is working.
    
    Test runs only if sed is present on the running system.  The test
    uses css with simple rules that are getting traslated for RTL locales.

--- test/io/template_cache_generator_spec.dart
+++ test/io/template_cache_generator_spec.dart
@@ -70,6 +70,45 @@ void main() {
       });
     });
 
+    if (new File("/bin/sed").existsSync()) {
+      it('should correctly use simple sed rewritter to fake css mirroring', () {
+        var tmpDir = Directory.systemTemp.createTempSync();
+        Future flush;
+        try {
+          flush = generator.main([
+              '--out=${tmpDir.path}/generated.dart',
+              '--url-rewrites=/test/io/test_files,rewritten',
+              '--css-rewriter=test/io/test_files/rewritter.sh',
+              'test/io/test_files/cssUrls/main.dart',
+              'generated']);
+        } catch(_) {
+          tmpDir.deleteSync(recursive: true);
+          rethrow;
+        }
+        return flush.then((_) {
+          expect(new File('${tmpDir.path}/generated.dart').readAsStringSync(),
+'''// GENERATED, DO NOT EDIT!
+library generated;
 
+import 'package:angular/angular.dart';
+
+primeTemplateCache(TemplateCache tc) {
+tc.put("rewritten/cssUrls/four.css", new HttpResponse(200, r""".float-right {
+  float: right;
+}
+.right-margin {
+  margin-right: 20px;
+}
+
+"""));
+tc.put("rewritten/cssUrls/one.css", new HttpResponse(200, r"""body {}"""));
+tc.put("rewritten/cssUrls/three.css", new HttpResponse(200, r"""body {}"""));
+tc.put("rewritten/cssUrls/two.css", new HttpResponse(200, r"""body {}"""));
+}''');
+        }).whenComplete(() {
+          tmpDir.deleteSync(recursive: true);
+        });
+      });
+    }
   });
 }
