53e6e79eb52156461748ad3be9746a4d4241ac1e
James deBoer <james@huronbox.com>
2014-05-16 18:14:29 -0700

    perf(interpolate): 20%. Cache the interpolated expressions.

--- lib/core/interpolate.dart
+++ lib/core/interpolate.dart
@@ -10,6 +10,7 @@ part of angular.core_internal;
  */
 @Injectable()
 class Interpolate implements Function {
+  var _cache = {};
   /**
    * Compiles markup text into expression.
    *
@@ -23,6 +24,15 @@ class Interpolate implements Function {
 
   String call(String template, [bool mustHaveExpression = false,
               String startSymbol = '{{', String endSymbol = '}}']) {
+    if (mustHaveExpression == false && startSymbol == '{{' && endSymbol == '}}') {
+      // cachable
+      return _cache.putIfAbsent(template, () => _call(template, mustHaveExpression, startSymbol, endSymbol));
+    }
+    return _call(template, mustHaveExpression, startSymbol, endSymbol);
+  }
+
+  String _call(String template, [bool mustHaveExpression = false,
+              String startSymbol, String endSymbol]) {
     if (template == null || template.isEmpty) return "";
 
     final startLen = startSymbol.length;
