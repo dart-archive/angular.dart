5ea7fe648eeb717ada1ae9025a0edfc659481c4e
Naomi Black <naomitraveller@gmail.com>
2014-05-20 17:18:21 -0700

    chore(travis): Fix Travis fetch for Dartium to handle redirects

--- scripts/travis/setup.sh
+++ scripts/travis/setup.sh
@@ -8,7 +8,8 @@ echo Fetch Dart channel: $CHANNEL
 DART_SDK_ZIP=dartsdk-linux-x64-release.zip
 
 echo http://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest/sdk/$DART_SDK_ZIP
-curl http://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest/sdk/$DART_SDK_ZIP > $DART_SDK_ZIP
+curl -L http://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest/sdk
+/$DART_SDK_ZIP > $DART_SDK_ZIP
 echo Fetched new dart version $(unzip -p $DART_SDK_ZIP dart-sdk/version)
 rm -rf dart-sdk
 unzip $DART_SDK_ZIP > /dev/null
