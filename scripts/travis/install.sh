9e784df06c49aa73b1592b898bbb0479de8ad754
Naomi Black <naomitraveller@gmail.com>
2014-05-20 17:18:21 -0700

    chore(travis): Fix Travis fetch for Dartium to handle redirects

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -4,7 +4,7 @@ set -e
 
 sh -e /etc/init.d/xvfb start
 
-DARTIUM_ZIP=http://storage.googleapis.com/dart-archive/channels/$CHANNEL/raw/latest/dartium/dartium-linux-x64-release.zip
+DARTIUM_ZIP=http://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest/dartium/dartium-linux-x64-release.zip
 FF_TAR=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$FIREFOX_VERSION/linux-x86_64/en-US/firefox-$FIREFOX_VERSION.tar.bz2
 CHROME_DEB=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
@@ -12,7 +12,7 @@ shopt -s nocasematch
 
 if [[ $BROWSERS =~ "dartium" ]]; then
   echo "Installing Dartium from $DARTIUM_ZIP"
-  curl $DARTIUM_ZIP > dartium.zip
+  curl -L $DARTIUM_ZIP > dartium.zip
   unzip dartium.zip > /dev/null
   rm -rf dartium
   rm dartium.zip
