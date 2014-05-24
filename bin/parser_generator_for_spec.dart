741bbc380aae21913752d15735522d337969a4a2
deploy-test@travis-ci.org <markovuksanovic@gmail.com>
2014-05-24 22:43:38 +1000

    fix(DynamicParser): Correctly handle throwing exceptions from method.
    Fixes #971

--- bin/parser_generator_for_spec.dart
+++ bin/parser_generator_for_spec.dart
@@ -96,6 +96,8 @@ main(arguments) {
       'map["square"] = 6',
       'method',
       'method()',
+      'causeException',
+      'causeException()',
       'notAFn()',
       'notmixed',
       'obj[0].name=1',
