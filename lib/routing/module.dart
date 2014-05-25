d267af6d419665f90925f34149bbae912a61aa05
Victor Berchet <victor@suumit.com>
2014-05-25 10:13:49 +0200

    refactor(routing): Make use of the latest API for docs & tests
    Closes #799

--- lib/routing/module.dart
+++ lib/routing/module.dart
@@ -24,7 +24,6 @@
  *   * `/recipe/:recipeId/view`
  *   * `/recipe/:recipeId/edit`
  *
- *
  * Let's try to define those routes in Angular. To get started we need to
  * provide an implementation of [RouteInitializerFn] function.
  *
@@ -37,29 +36,18 @@
  *
  *  Let's see how we could define our routes using the routing framework:
  *
- *      void initRoutes(Router router, RouteViewFactory view) {
- *        router.root
- *            ..addRoute(
- *                name: 'recipes',
- *                path: '/recipes',
- *                enter: view('recipes.html'))
- *            ..addRoute(
- *                name: 'addRecipe',
- *                path: '/addRecipe',
- *                enter: view('addRecipe.html'))
- *            ..addRoute(
- *                name: 'viewRecipe',
- *                path: '/recipe/:recipeId/view',
- *                enter: view('viewRecipe.html'))
- *            ..addRoute(
- *                name: 'editRecipe',
- *                path: '/recipe/:recipeId/edit',
- *                enter: view('editRecipe.html'));
+ *      void initRoutes(Router router, RouteViewFactory views) {
+ *        views.configure({
+ *            'recipes': ngRoute(path: '/recipes', view: 'recipes.html'),
+ *            'addRecipe': ngRoute(path: '/addRecipe', view: 'addRecipe.html'),
+ *            'viewRecipe': ngRoute(path: '/recipe/:recipeId/view', view: 'viewRecipe.html'),
+ *            'editRecipe': ngRoute(path: '/recipe/:recipeId/edit', view: 'editRecipe.html)
+ *        });
  *      }
  *
  *  We defined 4 routes and for each route we set views (templates) to be
  *  displayed when that route is "entered". For example, when the browser URL
- *  is set to `/recipes`, the `recipes.html` will be displayed.
+ *  is set to `/recipes`, the `recipes.html` template will be displayed.
  *
  *  You have to tell Angular where to load views by putting `<ng-view>` tag in
  *  you template.
@@ -112,11 +100,10 @@
  *  [RouteEvent.allowLeave].
  *
  *  Notice that we create a [RouteHandle] for our route. [RouteHandle] are
- *  a convinient wrapper around [Route] that makes unsubscribing route events
+ *  a convenient wrapper around [Route] that makes unsubscribing route events
  *  easier. For example, notice that we didn't need to manually call
  *  [StreamSubscription.cancel] for subscription to [Route.onLeave]. Calling
- *  [RouteHandle.discard] unsubscribes all listeneters created for the handle.
- *
+ *  [RouteHandle.discard] unsubscribes all listeners created for the handle.
  *
  * ## Hierarchical Routes
  *
@@ -124,29 +111,14 @@
  *  example we could have defined our routes like this:
  *
  *     void initRoutes(Router router, RouteViewFactory view) {
- *       router.root
- *           ..addRoute(
- *              name: 'recipes',
- *              path: '/recipes',
- *              enter: view('recipes.html'))
- *           ..addRoute(
- *              name: 'addRecipe',
- *              path: '/addRecipe',
- *              enter: view('addRecipe.html'))
- *           ..addRoute(
- *              name: 'recipe',
- *              path: '/recipe/:recipeId',
- *              mount: (Route route) => route
- *                  ..addRoute(
- *                      name: 'view',
- *                      path: '/view',
- *                      enter: view('viewRecipe.html'))
- *                  ..addRoute(
- *                      name: 'edit',
- *                      path: '/edit',
- *                      enter: view('editRecipe.html')));
- *     }
- *
+ *        views.configure({
+ *            'recipes': ngRoute(path: '/recipes', view: 'recipes.html'),
+ *            'addRecipe': ngRoute(path: '/addRecipe', view: 'addRecipe.html'),
+ *            'recipe': ngRoute(path: '/recipe/:recipeId', mount: {
+ *                'view': ngRoute(path: '/view', view: 'viewRecipe.html'),
+ *                'edit': ngRoute(path: '/edit', view: 'editRecipe.html),
+ *            })
+ *        });
  */
 library angular.routing;
 
