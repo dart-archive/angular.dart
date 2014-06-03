bb4bdc5f781c3e696d26163d00ba549601863ee7
James deBoer <james@huronbox.com>
2014-06-03 12:46:31 -0700

    feat(travis): Run against g3v1x as well

--- scripts/travis/setup.sh
+++ scripts/travis/setup.sh
@@ -13,6 +13,16 @@ echo Fetched new dart version $(unzip -p $DART_SDK_ZIP dart-sdk/version)
 rm -rf dart-sdk
 unzip $DART_SDK_ZIP > /dev/null
 
+if [ "$USE_G3" = "YES" ]; then
+  echo =============================================================================
+  echo "Rebasing onto g3v1x branch"
+  git config --global user.email "travis@travis"
+  git config --global user.name "AngularDart on Travis"
+  git remote add upstream https://github.com/angular/angular.dart.git
+  git fetch upstream
+  git rebase --onto upstream/g3v1x upstream/g3v1x-master
+fi
+
 echo =============================================================================
 . ./scripts/env.sh
 $DART --version
