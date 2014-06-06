b4d0f0f54603ad2cadf1aaefba3b756c2418ecdb
Chirayu Krishnappa <chirayu@chirayuk.com>
2014-06-06 12:15:44 -0700

    chore(reporter): record lexer benchmark results
    This initial version sends benchmark metrics to the backend server.
    
    Visit https://ng-dash.appspot.com/commit/fb2aa60a584dd4b2bbb3d30e8fcd4c588f8669ed
    or https://ng-dash.appspot.com/all to see an example.  (github:
    https://github.com/chirayuk/ng-dash)
    
    In this version, only Dart code run from the command line is able to
    record such reports.
    
    The code in benchmark/_reporter.dart handles sending the data to the
    server.  A future version will mode this code into a locally running
    proxy server enabling browser based Dart code to send such reports while
    still including the information from the build environment.
    
    Reading data from the report server does not require any auth.  See a
    simple example at https://github.com/chirayuk/ng-dash/commit/ex_01
    
    Writing data requires authentication.  Specifically, the REST server
    requires two cookies set:  user_email and user_secret.  (user_email does
    not have to be a valid e-mail just like AppEngine user e-mails.)  Of
    these, user_secret is meant to be secret and should be safeguarded.  For
    Travis, user_email is "travis-ci.org" and user_secret is encrypted in
    the .travis.yml.  Refer
    http://docs.travis-ci.com/user/build-configuration/#Secure-environment-variables
    for details on how this is done.

--- benchmark/lexer_perf.dart
+++ benchmark/lexer_perf.dart
@@ -1,20 +1,39 @@
 library lexer_perf;
 
 import '_perf.dart';
+import '_reporter.dart';
 import 'package:angular/core/parser/lexer.dart';
+
+
+Reporter reporter = new Reporter();
+var lexerStats = reporter.data.newChild("lexer", "lexer benchmarks");
+
+
+report(name, fn) {
+  var stats = lexerStats.newChild(name, "");
+  var metrics = statMeasure(fn);
+  stats.metrics["ops_per_sec"] = metrics.mean_ops_sec;
+  stats.metrics["variance"] = metrics.variance;
+  print('$name: => $metrics');
+  reporter.saveReport();
+}
+
 
 main() {
   Lexer lexer = new Lexer();
-  time('ident', () =>
+
+  report('ident', () =>
       lexer.call('ctrl foo baz ctrl.foo ctrl.bar ctrl.baz'));
-  time('ident-path', () =>
+  report('ident-path', () =>
       lexer.call('a.b a.b.c a.b.c.d a.b.c.d.e.f'));
-  time('num', () =>
+  report('num', () =>
       lexer.call('1 23 34 456 12341234 12351235'));
-  time('num-double', () =>
+  report('num-double', () =>
       lexer.call('.0 .1 .12 0.123 0.1234'));
-  time('string', () =>
+  report('string', () =>
       lexer.call("'quick brown dog and fox say what'"));
-  time('string-escapes', () =>
+  report('string-escapes', () =>
       lexer.call("quick '\\' brown \u1234 dog and fox\n\rsay what'"));
+
+  reporter.saveReport();
 }
