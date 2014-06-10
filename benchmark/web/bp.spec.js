0c996c681e6dea59bd2239f809800c0ba99091ed
Jeff Cross <middlefloor@gmail.com>
2014-06-10 09:44:23 -0700

    test(benchmark): add test runner and stubs for bp.js
    This commit adds a karma setup with a spec file for bp.js, and
    has refactored bp.js to expose all functions on a global "bp" object.
    
    Behavior of functions hasn't changed, other than to remove some
    duplication of code regarding adding buttons to the DOM.

---
+++ benchmark/web/bp.spec.js
@@ -0,0 +1,51 @@
+describe('bp', function() {
+  var bp = window.bp;
+
+  beforeEach(function() {
+    bp._container = document.createElement('div');
+  });
+
+  describe('.loopBenchmark()', function() {
+
+  });
+
+
+  describe('.onceBenchmark()', function() {
+
+  });
+
+
+  describe('.runBenchmarkSteps()', function() {
+
+  });
+
+
+  describe('.container()', function() {
+
+  });
+
+
+  describe('.addButton()', function() {
+
+  });
+
+
+  describe('.addLinks()', function() {
+
+  });
+
+
+  describe('.addInfo()', function() {
+
+  });
+
+
+  describe('.calcStats()', function() {
+
+  });
+
+
+  describe('.onDOMContentLoaded()', function() {
+
+  });
+});
