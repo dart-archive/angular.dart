b2c050b35b5fe7d85e50e008c935d7ada9898197
Jeff Cross <middlefloor@gmail.com>
2014-06-10 09:44:23 -0700

    test(benchmark): add test runner and stubs for bp.js
    This commit adds a karma setup with a spec file for bp.js, and
    has refactored bp.js to expose all functions on a global "bp" object.
    
    Behavior of functions hasn't changed, other than to remove some
    duplication of code regarding adding buttons to the DOM.

--- benchmark/web/bp.js
+++ benchmark/web/bp.js
@@ -1,11 +1,112 @@
-window.benchmarkSteps = [];
+var bp = window.bp = {};
+bp.steps = window.benchmarkSteps = [];
+bp.timesPerAction = {};
+bp.running = false;
+bp.numSamples = 10;
 
-window.addEventListener('DOMContentLoaded', function() {
-  var container = document.querySelector('#benchmarkContainer');
+bp.numMilliseconds = function() {
+  if (window.performance != null && typeof window.performance.now == 'function') {
+    return window.performance.now();
+  } else if (window.performance != null && typeof window.performance.webkitNow == 'function') {
+    return window.performance.webkitNow();
+  } else {
+    console.log('using Date.now');
+    return Date.now();
+  }
+};
 
+bp.loopBenchmark = function loopBenchmark() {
+  if (bp.running) {
+    bp.loopBtn.innerText = 'Loop';
+    bp.running = false;
+  } else {
+    window.requestAnimationFrame(function() {
+      bp.loopBtn.innerText = 'Pause';
+      bp.running = true;
+      var loopB = function() {
+        if (bp.running) {
+          window.requestAnimationFrame(function() {
+            if (bp.running) bp.runBenchmarkSteps(loopB);
+          });
+        }
+      };
+      loopB();
+    });
+  }
+};
+
+bp.onceBenchmark = function() {
+  var btn = bp.onceBtn;
+  window.requestAnimationFrame(function() {
+    btn.innerText = '...';
+    window.requestAnimationFrame(function() {
+      bp.runBenchmarkSteps(function() {
+        btn.innerText = 'Once';
+      });
+    });
+  });
+};
+
+bp.runBenchmarkSteps = function runBenchmarkSteps(done) {
+  // Run all the steps;
+  var times = {};
+  bp.steps.forEach(function(bs) {
+    if (typeof window.gc === 'function') {
+      window.gc();
+    }
+    var startTime = bp.numMilliseconds();
+    bs.fn();
+    times[bs.name] = bp.numMilliseconds() - startTime;
+  });
+  bp.calcStats(times);
+
+  done();
+};
+
+bp.calcStats = function calcStats(times) {
+  var iH = '';
+  bp.steps.forEach(function(bs) {
+    var tpa = bp.timesPerAction[bs.name];
+    if (!tpa) {
+      tpa = bp.timesPerAction[bs.name] =  {
+        times: [], // circular buffer
+        fmtTimes: [],
+        nextEntry: 0
+      }
+    }
+    tpa.fmtTimes[tpa.nextEntry] = ('' + times[bs.name]).substr(0,6);
+    tpa.times[tpa.nextEntry++] = times[bs.name];
+    tpa.nextEntry %= bp.numSamples;
+    var avg = 0;
+    tpa.times.forEach(function(x) { avg += x; });
+    avg /= Math.min(bp.numSamples, tpa.times.length);
+    avg = ('' + avg).substr(0,6);
+    iH += '<div>' + ('         ' + bs.name).slice(-10).replace(/ /g, '&nbsp;') + ': avg-' + bp.numSamples + ':<b>' + avg + 'ms</b> [' + tpa.fmtTimes.join(', ') + ']ms</div>';
+  });
+  bp.infoDiv.innerHTML = iH;
+};
+
+bp.container = function() {
+  if (!bp._container) {
+    bp._container = document.querySelector('#benchmarkContainer');
+  }
+  return bp._container;
+}
+
+bp.addButton = function(reference, text, handler) {
+  var container = bp.container();
+  bp[reference] = document.createElement('button');
+  bp[reference].innerText = text;
+
+  bp[reference].addEventListener('click', handler);
+
+  container.appendChild(bp[reference]);
+}
+
+bp.addLinks = function() {
   // Add links to everything
   var linkDiv = document.createElement('div');
-  linkDiv.style['margin-bottom'] = "1.5em";
+  linkDiv.style['margin-bottom'] = '1.5em';
   var linkHtml = [
     '<style>',
     '.bpLink { background: lightblue; padding: 1em }',
@@ -14,8 +115,8 @@ window.addEventListener('DOMContentLoaded', function() {
   ].join('\n');
 
   [
-      // Add new benchmark suites here
-      ['tree.html', 'TreeComponent']
+    // Add new benchmark suites here
+    ['tree.html', 'TreeComponent']
   ].forEach((function (link) {
     linkHtml += [
       '<a class=bpLink href=',
@@ -27,113 +128,21 @@ window.addEventListener('DOMContentLoaded', function() {
   }));
 
   linkDiv.innerHTML = linkHtml;
-  container.appendChild(linkDiv);
+  bp.container().appendChild(linkDiv);
+};
 
+bp.addInfo = function() {
+  bp.infoDiv = document.createElement('div');
+  bp.infoDiv.style['font-family'] = 'monospace';
+  bp.container().appendChild(bp.infoDiv);
+};
 
-  // Benchmark runner
-  var btn = document.createElement('button');
-  btn.innerText = "Loop";
-  var running = false;
-  btn.addEventListener('click', loopBenchmark);
+bp.onDOMContentLoaded = function() {
+  bp.addLinks();
+  bp.addButton('loopBtn', 'Loop', bp.loopBenchmark);
+  bp.addButton('onceBtn', 'Once', bp.onceBenchmark);
 
-  container.appendChild(btn);
+  bp.addInfo();
+};
 
-  function loopBenchmark() {
-    if (running) {
-      btn.innerText = "Loop";
-      running = false;
-    } else {
-      window.requestAnimationFrame(function() {
-        btn.innerText = "Pause";
-        running = true;
-        var loopB = function() {
-          if (running) {
-            window.requestAnimationFrame(function() {
-              if (running) runBenchmarkSteps(loopB);
-            });
-          }
-        };
-        loopB();
-      });
-    }
-  }
-
-
-  var onceBtn = document.createElement('button');
-  onceBtn.innerText = "Once";
-  onceBtn.addEventListener('click', function() {
-    window.requestAnimationFrame(function() {
-      onceBtn.innerText = "...";
-      window.requestAnimationFrame(function() {
-        runBenchmarkSteps(function() {
-          onceBtn.innerText = "Once";
-        });
-      });
-    });
-  });
-  container.appendChild(onceBtn);
-
-  var infoDiv = document.createElement('div');
-  infoDiv.style['font-family'] = 'monospace';
-  container.appendChild(infoDiv);
-
-
-  var numMilliseconds;
-  var performance = window.performance;
-  if (performance != null && typeof performance.now == "function") {
-    numMilliseconds = function numMillisecondsWPN() {
-      return performance.now();
-    }
-  } else if (performance != null && typeof performance.webkitNow == "function") {
-    numMilliseconds = function numMillisecondsWebkit() {
-      return performance.webkitNow();
-    }
-  } else {
-    console.log('using Date.now');
-    numMilliseconds = function numMillisecondsDateNow() {
-      return Date.now();
-    };
-  }
-
-  function runBenchmarkSteps(done) {
-    // Run all the steps;
-    var times = {};
-    window.benchmarkSteps.forEach(function(bs) {
-      if (typeof window.gc === 'function') {
-        window.gc();
-      }
-      var startTime = numMilliseconds();
-      bs.fn();
-      times[bs.name] = numMilliseconds() - startTime;
-    });
-    calcStats(times);
-
-    done();
-  }
-
-  var timesPerAction = {};
-
-  var NUM_SAMPLES = 10;
-  function calcStats(times) {
-    var iH = '';
-    window.benchmarkSteps.forEach(function(bs) {
-      var tpa = timesPerAction[bs.name];
-      if (!tpa) {
-        tpa = timesPerAction[bs.name] =  {
-          times: [], // circular buffer
-          fmtTimes: [],
-          nextEntry: 0
-        }
-      }
-      tpa.fmtTimes[tpa.nextEntry] = ('' + times[bs.name]).substr(0,6);
-      tpa.times[tpa.nextEntry++] = times[bs.name];
-      tpa.nextEntry %= NUM_SAMPLES;
-      var avg = 0;
-      tpa.times.forEach(function(x) { avg += x; });
-      avg /= Math.min(NUM_SAMPLES, tpa.times.length);
-      avg = ('' + avg).substr(0,6);
-      iH += '<div>' + ('         ' + bs.name).slice(-10).replace(/ /g, '&nbsp;') + ': avg-' + NUM_SAMPLES + ':<b>' + avg + 'ms</b> [' + tpa.fmtTimes.join(', ') + ']ms</div>';
-    });
-    infoDiv.innerHTML = iH;
-  }
-});
+window.addEventListener('DOMContentLoaded', bp.onDOMContentLoaded);
