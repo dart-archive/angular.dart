f738634a2f6e8fc81dcd50964c7ede68056e4206
James deBoer <james@huronbox.com>
2014-06-18 12:30:36 -0700

    feat(cache): Move cache out of core, add a CacheRegister

---
+++ lib/cache/module.dart
@@ -0,0 +1,16 @@
+library angular.cache;
+
+import 'dart:collection';
+import 'dart:async';
+
+import 'package:di/di.dart';
+import 'package:angular/core/annotation_src.dart';
+
+part "cache.dart";
+part "cache_register.dart";
+
+class CacheModule extends Module {
+  CacheModule() {
+    bind(CacheRegister);
+  }
+}
