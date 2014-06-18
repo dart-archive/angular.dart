6ac8caccbe4a765cc0cf81cd2f7f361f75cbfd89
deploy-test@travis-ci.org <markovuksanovic@gmail.com>
2014-06-18 22:40:56 +1000

    feat(Http): Http can be configured to coalesce the requests.

--- test/core_dom/http_spec.dart
+++ test/core_dom/http_spec.dart
@@ -1422,6 +1422,39 @@ void main() {
         });
       });
     });
+
+    describe('coalesce', () {
+      beforeEachModule((Module module) {
+        module.bind(HttpConfig, toFactory: (i) => new HttpConfig(
+                coalesce: true, coalesceDuration: new Duration(milliseconds: 100)));
+      });
+
+      it('should coalesce requests', async((Http http) {
+        backend.expect('GET', '/foo').respond(200, 'foo');
+        backend.expect('GET', '/bar').respond(200, 'bar');
+
+        var fooResp, barResp;
+        http.get('/foo').then((HttpResponse resp) => fooResp = resp.data);
+        http.get('/bar').then((HttpResponse resp) => barResp = resp.data);
+
+        microLeap();
+        backend.flush();
+        microLeap();
+        expect(fooResp).toBeNull();
+        expect(barResp).toBeNull();
+
+        clockTick(milliseconds: 99);
+        microLeap();
+        expect(fooResp).toBeNull();
+        expect(barResp).toBeNull();
+
+        clockTick(milliseconds: 1);
+        microLeap();
+        expect(fooResp).toEqual('foo');
+        expect(barResp).toEqual('bar');
+      }));
+
+    });
   });
 }
 
