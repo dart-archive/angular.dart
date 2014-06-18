5623129da68965ea917f4cd94aa18c20aa4b531e
Victor Savkin <vic.savkin@gmail.com>
2014-06-18 15:18:06 -0400

    chore(travis): fail build if a test has been 'iit'ed
    Change the build script to fail if a test was 'itt'ed or 'ddescribe'ed.
    
    Closes #922
    
    Closes #1172

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -111,7 +111,18 @@ echo BROWSER=$BROWSERS
 $NGDART_BASE_DIR/node_modules/jasmine-node/bin/jasmine-node playback_middleware/spec/ &&
 node "node_modules/karma/bin/karma" start karma.conf \
     --reporters=junit,dots --port=8765 --runner-port=8766 \
-    --browsers=$BROWSERS --single-run --no-colors
+    --browsers=$BROWSERS --single-run --no-colors 2>&1 | tee karma-output.log
+
+if grep -q "WARN: iit" karma-output.log; then
+  echo "ERROR: iit caused some tests to be excluded"
+  exit 1
+fi
+
+if grep -q "WARN: ddescribe" karma-output.log; then
+  echo "ERROR: ddescribe caused some tests to be excluded"
+  exit 1
+fi
+
 
 
 echo '-------------------------'
