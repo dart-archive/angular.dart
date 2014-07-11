b8dcaa8db7e3cf0980009d2786beb31a1ed953ee
deploy-test@travis-ci.org <markovuksanovic@gmail.com>
2014-07-12 00:28:02 +1000

    MicroIterable

--- test/collection/micro_iterable_spec.dart
+++ test/collection/micro_iterable_spec.dart
@@ -14,5 +14,33 @@ void main() {
     it('should have length', () {
       expect(iterable.length).toBe(8);
     });
+
+    it('should be able to get a list', () {
+      var list = iterable.toList();
+      expect(list).toEqual([1,2,3,4,5,6,7,8]);
+      expect(() {
+        list.add('foo');
+      }).not.toThrow();
+    });
+
+    it('should be able to get a fixed size list', () {
+      var list = iterable.toList(growable: false);
+      expect(list).toEqual([1,2,3,4,5,6,7,8]);
+      expect(() {
+        list.add('foo');
+      }).toThrow();
+    });
+
+    it('should take specified number of elements', () {
+      expect(iterable.take(4)).toEqual([1,2,3,4]);
+    });
+
+    it('should join elements', () {
+      expect(iterable.join(", ")).toEqual("1, 2, 3, 4, 5, 6, 7, 8");
+    });
+
+    it('should work with methods provided by IterableMixin', () {
+      expect(iterable.takeWhile((value) => value < 6)).toEqual([1,2,3,4,5]);
+    });
   });
 }
