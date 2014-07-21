467c046fc70f689a1bd905bdfe0ffa5bc104704b
vsavkin <vic.savkin@gmail.com>
2014-07-21 13:40:24 -0700

    reflactor(parser): rename parser_static and parser_dynamic into static_closure_map and dynamic_closure_map
    Closes #1250

--- lib/application_factory_static.dart
+++ lib/application_factory_static.dart
@@ -33,7 +33,7 @@ library angular.app.factory.static;
 import 'package:angular/application.dart';
 import 'package:angular/core/registry.dart';
 import 'package:angular/core/parser/parser.dart';
-import 'package:angular/core/parser/parser_static.dart';
+import 'package:angular/core/parser/static_closure_map.dart';
 import 'package:angular/core/parser/dynamic_parser.dart';
 import 'package:angular/core/registry_static.dart';
 import 'package:angular/change_detection/change_detection.dart';
