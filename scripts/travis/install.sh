8cefb85cad600506771039346aad9a1766d66366
Chirayu Krishnappa <chirayu@chirayuk.com>
2014-08-20 16:24:23 -0700

    chore(travis): fix failures for g3stable/g3v1x. bail early.
    - bail early when we can.
    - g3v1x requires a newer version of Dart than 1.5.8.  Quickly mark the
      job as successful if we're testing something that's descended from the
      g3v1x branch and we're attempting to use the SDK at this version.
    - the g3stable jobs do not make sense if one is testing a branch
      descended from g3v1x.  Bail early with success for those jobs.

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -2,7 +2,47 @@
 
 set -e -o pipefail
 
-sh -e /etc/init.d/xvfb start
+# Fail fasts.
+
+git merge-base --is-ancestor 44577768e6bd4ac649703f6172c2490bce4f9132 HEAD && \
+  G3V1X_LINEAGE=1 || G3V1X_LINEAGE=0
+
+if [[ "$USE_G3" == "YES" && "$G3V1X_LINEAGE" == "1" ]]; then
+  exec > >(tee SKIP_TRAVIS_TESTS)
+  echo '==================================================================='
+  echo '== SKIPPING script: The current SHA is already a descendent of   =='
+  echo '== g3v1x making USE_G3 redundant.                                =='
+  echo '==================================================================='
+  exit 0
+fi
+
+AVAILABLE_DART_VERSION=$(curl "https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest/VERSION" | python -c \
+    'import sys, json; print(json.loads(sys.stdin.read())["version"])')
+
+# g3v1x and Dart 1.5.8
+# --------------------
+# Skip tests for branches based on the g3v1x channel with Dart 1.5.8.
+# g3v1x uses dependency overrides that cannot be satisfied by the pub tool
+# shipper in Dart version 1.5.8.
+#
+# Ref: https://travis-ci.org/angular/angular.dart/jobs/33106780
+# 
+#   Dart VM version: 1.5.8 (Tue Jul 29 07:05:41 2014) on "linux_x64"
+#   Resolving dependencies... 
+#   Incompatible version constraints on barback:
+#   - angular 0.13.0 depends on version 0.14.1+3
+#   - pub itself depends on version >=0.13.0 <0.14.1
+if [[ "$USE_G3" == "YES" && "$AVAILABLE_DART_VERSION" == "1.5.8" ]]; then
+  exec > >(tee SKIP_TRAVIS_TESTS)
+  echo '==================================================================='
+  echo '== SKIPPING script: The g3stable job is rebased on the g3v1x     =='
+  echo '== branch.  The g3v1x branch cannot be tested with Dart          =='
+  echo '== version 1.5.8.  The dependency overrides require a newer      =='
+  echo '== version of Dart.                                              =='
+  echo '== Ref: https://travis-ci.org/angular/angular.dart/jobs/33106780 =='
+  echo '==================================================================='
+  exit 0
+fi
 
 DARTIUM_ZIP=http://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest/dartium/dartium-linux-x64-release.zip
 
@@ -23,18 +63,8 @@ DART_SDK_ZIP=dartsdk-linux-x64-release.zip
 
 # TODO(chirayu): Remove this once issue 1350 is fixed.
 SVN_REVISION=latest
-if [[ "$CHANNEL" == "dev" ]]; then
-  issue_1350_state=$(curl 'https://api.github.com/repos/angular/angular.dart/issues/1020' | python -c \
-                     'import sys, json; print(json.loads(sys.stdin.read())["state"])')
-  if [[ "$issue_1350_state" == "open" ]]; then
-    # Is there a newer dev SDK?
-    latest_version=$(curl 'https://storage.googleapis.com/dart-archive/channels/dev/release/latest/VERSION' | python -c \
-      'import sys, json; print(json.loads(sys.stdin.read())["version"])')
-    if [[ "$latest_version" == "1.6.0-dev.8.0" ]]; then
-      # Use prior working version (1.6.0-dev.7.0)
-      SVN_REVISION=38831
-    fi
-  fi
+if [[ "$AVAILABLE_DART_VERSION" == "1.6.0-dev.8.0" ]]; then
+  SVN_REVISION=38831  # Use prior working version (1.6.0-dev.7.0)
 fi
 
 URL=https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/$SVN_REVISION/sdk/$DART_SDK_ZIP
@@ -42,5 +72,6 @@ echo $URL
 curl -L -O $URL
 
 echo Fetched new dart version $(unzip -p $DART_SDK_ZIP dart-sdk/version)
-rm -rf dart-sdk
-unzip $DART_SDK_ZIP > /dev/null
+unzip -q $DART_SDK_ZIP
+
+sh -e /etc/init.d/xvfb start
