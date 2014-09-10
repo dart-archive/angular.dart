88db9db44e290ac88fa98adb1452610cf620221b
Chirayu Krishnappa <chirayu@chirayuk.com>
2014-09-09 20:10:19 -0700

    test(e2e): run on Dartium, dart2js and sauce browsers
    Command line runs will test Dartium by default if no BROWSERS are
    specified.  If BROWSERS are specified, they all have to be local
    browsers or all sauce browsers.  If you're only specifying sauce
    browsers (e.g. SL_Chrome, SL_Firefox, etc.), you may optionally also
    specify DartiumWithWebPlatform and this will run two instances of
    Protractor.  (This is a limitation of Protractor's config.)
    
    On Travis, the job for a specific SDK version tests both
    DartiumWithWebPlatform as well as SL_Chrome.  SL_Firefox is currently
    disabled because it's failing.  Note that prior to this commit, the
    BROWSERS variable was always ignored.

--- test_transformers/transformersE2eConf.js
+++ test_transformers/transformersE2eConf.js
@@ -18,16 +18,9 @@ var config = {
 
     specs: ['relative_uris_spec.dart'],
 
-    multiCapabilities: [{
-      browserName: 'chrome',
-      chromeOptions: configQuery.getChromeOptions(),
-      count: 1
-    }],
-
     baseUrl: configQuery.getBaseUrl({
       envVar: "TEST_TRANSFORMERS_BASEURL"
     }),
-
 
     jasmineNodeOpts: {
       isVerbose: true, // display spec names.
@@ -37,17 +30,9 @@ var config = {
     },
 };
 
-// Saucelabs case.
-if (process.env.SAUCE_USERNAME != null) {
-  config.sauceUser = process.env.SAUCE_USERNAME;
-  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.seleniumAddress = null;
-
-  config.multiCapabilities.forEach(function(capability) {
-    capability['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
-    capability['build'] = process.env.TRAVIS_BUILD_NUMBER;
-    capability['name'] = 'AngularDart Transformers E2E Suite';
-  });
-}
+configQuery.updateConfigForBrowsers(config, process.env.BROWSERS.split(","), 1);
+config.multiCapabilities.forEach(function(capability) {
+	capability['name'] = 'AngularDart Transformers E2E Suite';
+});
 
 exports.config = config;
