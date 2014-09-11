22ab40f4b8f1bcf0c7178ce569c29ba250d287f6
Chirayu Krishnappa <chirayu@chirayuk.com>
2014-09-11 14:57:41 -0700

    style(protractor): fix indentation and style in config files

--- test_e2e/configQuery.js
+++ test_e2e/configQuery.js
@@ -1,45 +1,43 @@
 var env = process.env,
-fs = require('fs'),
-path = require('path');
-
-
-var runningOnTravis = (env.TRAVIS !== undefined);
+    fs = require('fs'),
+    path = require('path'),
+    runningOnTravis = (env.TRAVIS !== undefined);
 
 
 function getBaseUrl(configWithEnvKey) {
     if (env[configWithEnvKey.envVar]) {
-	return env[configWithEnvKey.envVar];
+      return env[configWithEnvKey.envVar];
     } else if (env.USER == 'chirayu') {
-	return 'http://example.ngdart.localhost';
+      return 'http://example.ngdart.localhost';
     } else {
-    // Default host:port when you run "pub serve" from the example
-    // subdirectory of the AngularDart repo.
-	return 'http://localhost:8080';
+      // Default host:port when you run "pub serve" from the example
+      // subdirectory of the AngularDart repo.
+      return 'http://localhost:8080';
     }
 }
 
 
 function getDartiumBinary() {
-    var ensure = function(condition) {
-	if (!condition) throw "Unable to locate Dartium.  Please set the DARTIUM environment variable.";
+    function ensure(condition) {
+      if (!condition) throw "Unable to locate Dartium.  Please set the DARTIUM environment variable.";
     };
 
     if (env.DARTIUM) {
-	return env.DARTIUM;
+      return env.DARTIUM;
     }
     var platform = require('os').platform();
     var DART_SDK = env.DART_SDK;
     if (DART_SDK) {
-    // Locate the chromium directory as a sibling of the DART_SDK
-    // directory.  (It's there if you unpacked the full Dart distribution.)
-	var chromiumRoot = path.join(DART_SDK, "../chromium");
-	ensure(fs.existsSync(chromiumRoot));
-	var binary = path.join(chromiumRoot,
-			       (platform == 'darwin') ? 'Chromium.app/Contents/MacOS/Chromium' : 'chrome');
-	ensure(fs.existsSync(binary));
-	return binary;
+      // Locate the chromium directory as a sibling of the DART_SDK
+      // directory.  (It's there if you unpacked the full Dart distribution.)
+      var chromiumRoot = path.join(DART_SDK, "../chromium");
+      ensure(fs.existsSync(chromiumRoot));
+      var binary = path.join(chromiumRoot,
+          (platform == 'darwin') ? 'Chromium.app/Contents/MacOS/Chromium' : 'chrome');
+      ensure(fs.existsSync(binary));
+      return binary;
     }
-  // Last resort: Try the standard location on Macs for the AngularDart team.
+    // Last resort: Try the standard location on Macs for the AngularDart team.
     var binary = '/Applications/dart/chromium/Chromium.app/Contents/MacOS/Chromium';
     ensure(platform == 'darwin' && fs.existsSync(binary));
     return binary;
@@ -48,21 +46,21 @@ function getDartiumBinary() {
 
 function getChromeOptions() {
     if (!runningOnTravis) {
-	return {'binary': getDartiumBinary()};
+      return {'binary': getDartiumBinary()};
     }
-  // In Travis, the list of browsers to test is specified as a CSV in the
-  // BROWSERS environment variable.
-  // TODO(chirayu): Parse the BROWSERS csv so we also test on Firefox.
+    // In Travis, the list of browsers to test is specified as a CSV in the
+    // BROWSERS environment variable.
+    // TODO(chirayu): Parse the BROWSERS csv so we also test on Firefox.
     if (env.TESTS == "vm") {
-	return {'binary': env.DARTIUM_BIN};
+      return {'binary': env.DARTIUM_BIN};
     }
     if (env.TESTS == "dart2js") {
-	return {
-	    'binary': env.CHROME_BIN,
-      // Ref: https://github.com/travis-ci/travis-ci/issues/938
-      //      https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
-	    'args': ['no-sandbox=true']
-	};
+      return {
+          'binary': env.CHROME_BIN,
+          // Ref: https://github.com/travis-ci/travis-ci/issues/938
+          //      https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
+          'args': ['no-sandbox=true']
+      };
     }
     throw new Error("Unknown Travis configuration specified by TESTS variable");
 }
--- test_transformers/transformersE2eConf.js
+++ test_transformers/transformersE2eConf.js
@@ -19,35 +19,35 @@ var config = {
     specs: ['relative_uris_spec.dart'],
 
     multiCapabilities: [{
-	'browserName': 'chrome',
-	'chromeOptions': configQuery.getChromeOptions(),
-	count: 1
+      browserName: 'chrome',
+      chromeOptions: configQuery.getChromeOptions(),
+      count: 1
     }],
 
     baseUrl: configQuery.getBaseUrl({
-	envVar: "TEST_TRANSFORMERS_BASEURL"
+      envVar: "TEST_TRANSFORMERS_BASEURL"
     }),
 
 
     jasmineNodeOpts: {
-	isVerbose: true, // display spec names.
-	showColors: true, // print colors to the terminal.
-	includeStackTrace: true, // include stack traces in failures.
-	defaultTimeoutInterval: 80000 // wait time in ms before failing a test.
+      isVerbose: true, // display spec names.
+      showColors: true, // print colors to the terminal.
+      includeStackTrace: true, // include stack traces in failures.
+      defaultTimeoutInterval: 80000 // wait time in ms before failing a test.
     },
 };
 
 // Saucelabs case.
 if (process.env.SAUCE_USERNAME != null) {
-    config.sauceUser = process.env.SAUCE_USERNAME;
-    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-    config.seleniumAddress = null;
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.seleniumAddress = null;
 
-    config.multiCapabilities.forEach(function(capability) {
-	capability['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
-	capability['build'] = process.env.TRAVIS_BUILD_NUMBER;
-	capability['name'] = 'AngularDart Transformers E2E Suite';
-    });
+  config.multiCapabilities.forEach(function(capability) {
+    capability['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
+    capability['build'] = process.env.TRAVIS_BUILD_NUMBER;
+    capability['name'] = 'AngularDart Transformers E2E Suite';
+  });
 }
 
 exports.config = config;
