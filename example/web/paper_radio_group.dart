a511c1a368eba198b3b54b630d9c70c9e9d96ab0
Victor Berchet <victor@suumit.com>
2014-09-19 11:43:51 +0200

    chore: install paper_elements & core_elements from pub
    fixes #1281
    fixes #1283

--- example/web/paper_radio_group.dart
+++ example/web/paper_radio_group.dart
@@ -1,13 +1,14 @@
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
-import 'dart:html';
-
 @Component(
   selector: 'cmp',
-  template: '<paper-radio-group bind-selected="cmp.insertOption"> <paper-radio-button name="asis" label="Insert as is"></paper-radio-button> <paper-radio-button name="duplicate" label="Insert as duplicate"></paper-radio-button> </paper-radio-group> {{cmp.insertOption}}',
-  publishAs: 'cmp'
-)
+  template: '''
+    <paper-radio-group bind-selected="cmp.insertOption">
+      <paper-radio-button name="asis" label="Insert as is"></paper-radio-button>
+      <paper-radio-button name="duplicate" label="Insert as duplicate"></paper-radio-button>
+    </paper-radio-group> {{cmp.insertOption}}''',
+  publishAs: 'cmp')
 class Cmp {
   Object insertOption;
 }
