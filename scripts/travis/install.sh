0712a2a50ec489914eaa2c144f6b876a872e13ea
Chirayu Krishnappa <chirayu@chirayuk.com>
2014-10-02 11:23:52 -0700

    chore(travis): remove dart version blacklist for 1.7.0-dev.1.0
    Closes #1443

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -21,13 +21,7 @@ AVAILABLE_DART_VERSION=$(curl "https://storage.googleapis.com/dart-archive/chann
 
 echo Fetch Dart channel: $CHANNEL
 
-SVN_REVISION=latest
-# TODO(chirayu): Remove this once issue 20896 is fixed.
-if [[ "$AVAILABLE_DART_VERSION" == "1.7.0-dev.1.0" ]]; then
-  SVN_REVISION=39661  # Use prior working version (1.7.0-dev.0.1)
-fi
-
-URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/$SVN_REVISION
+URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest
 DART_SDK_URL=$URL_PREFIX/sdk/dartsdk-linux-x64-release.zip
 if [[ "${BROWSERS,,}" =~ "dartium" ]]; then
   DARTIUM_URL=$URL_PREFIX/dartium/dartium-linux-x64-release.zip
