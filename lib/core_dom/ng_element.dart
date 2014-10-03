5deafbe873b8117e78831db4f00e007eed86813a
vsavkin <vic.savkin@gmail.com>
2014-10-03 19:59:40 -0400

    doc(NgElement): add docs to NgElement

--- lib/core_dom/ng_element.dart
+++ lib/core_dom/ng_element.dart
@@ -1,5 +1,23 @@
 part of angular.core.dom_internal;
 
+/**
+ * An [NgElement] wraps a DOM element. It can be used by a component to
+ * modify the element's classes and attributes.
+ *
+ * Example:
+ *
+ *    class ElementAwareComponent {
+ *      NgElement ngElement;
+ *      ElementAwareComponent(this.ngElement);
+ *
+ *      void onClick() {
+ *        ngElement.addClass("clicked");
+ *      }
+ *    }
+ *
+ * These modifications are asynchronous and will update the DOM only
+ * during the next flush phase.
+ */
 @Injectable()
 class NgElement {
   static const _TO_BE_REMOVED = const Object();
@@ -16,21 +34,25 @@ class NgElement {
 
   NgElement(this.node, this._rootScope, this._animate, [this._lightDom]);
 
+  /// Schedules a DOM write adding [className] to the element.
   void addClass(String className) {
     _scheduleDomWrite();
     _classesToUpdate[className] = true;
   }
 
+  /// Schedules a DOM write removing [className] from the element.
   void removeClass(String className) {
     _scheduleDomWrite();
     _classesToUpdate[className] = false;
   }
 
+  /// Schedules a DOM write updating [attrName] to [value].
   void setAttribute(String attrName, [value = '']) {
     _scheduleDomWrite();
     _attributesToUpdate[attrName] = value == null ? '' : value;
   }
 
+  /// Schedules a DOM write removing [attrName] from the element.
   void removeAttribute(String attrName) {
     _scheduleDomWrite();
     _attributesToUpdate[attrName] = _TO_BE_REMOVED;
